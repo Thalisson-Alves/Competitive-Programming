0f889dd95f4e4b1c70435b669db1a9435ce70e71
Thalisson-Alves <thalisson2030@gmail.com>
2023-05-21 17:07:27 -0300

    add gnu ordered_set

--- template.cpp
+++ template.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
  
 #ifdef DEBUG
 #include "debug.cpp"
@@ -25,7 +31,7 @@ int main()
   cout.tie(nullptr);
  
   int t = 1;
-  // cin >> t;
+  // cin >> t; cin.ignore();
   for (auto i = 1; i <= t; i++)
     solve();
  
