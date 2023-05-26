0d9fd9918f9500f65eb8bfa4907d19e22a962e7f
Thalisson-Alves <thalisson2030@gmail.com>
2023-05-26 10:33:31 -0300

    add sieve

---
+++ snips/sieve.cpp
@@ -0,0 +1,11 @@
+ll MAX_N = 1e6;
+vector<char> is_prime(MAX_N+1, true);
+void sieve()
+{
+  is_prime[0] = is_prime[1] = false;
+  for (ll i = 2; i <= MAX_N; i++)
+  {
+    for (ll j = i * i; j <= MAX_N; j+=i)
+      is_prime[j] = false;
+  }
+}
