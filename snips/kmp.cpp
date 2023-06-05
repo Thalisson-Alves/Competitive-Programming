e7e2f46be5b41abc35f426c486ea852171931b85
Thalisson-Alves <thalisson2030@gmail.com>
2023-06-05 09:50:53 -0300

    add Knuth Morris Pratt

---
+++ snips/kmp.cpp
@@ -0,0 +1,32 @@
+vector<int> kmp_border(const string &p)
+{
+  int m = (int)p.size(), t = -1;
+  vector<int> bs(m + 1, -1);
+
+  for (int i = 1; i <= m; i++)
+  {
+    for (; t > -1 and p[t] != p[i - 1]; t = bs[t]);
+    ++t;
+    bs[i] = (i == m or p[t] != p[i]) ? t : bs[t];
+  }
+
+  return bs;
+}
+
+int kmp(const string &s, const string &p)
+{
+  int n = (int)s.size(), m = (int)p.size(), i = 0, j = 0, occ = 0;
+  auto bs = kmp_border(p);
+
+  while (i <= n - m)
+  {
+    for (; j < m and p[j] == s[i + j]; ++j);
+    if (j == m) ++occ;
+
+    int shift = j - bs[j];
+    i += shift;
+    j = max(0, j - shift);
+  }
+
+  return occ;
+}
