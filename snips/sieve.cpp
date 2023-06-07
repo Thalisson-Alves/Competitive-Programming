ff6333eb5c4510d7c229e87f108284314be05637
Thalisson-Alves <thalisson2030@gmail.com>
2023-06-07 19:40:48 -0300

    add primes vector

--- snips/sieve.cpp
+++ snips/sieve.cpp
@@ -1,11 +1,13 @@
 ll MAX_N = 1e6;
 vector<char> is_prime(MAX_N+1, true);
+vector<ll> primes;
 void sieve()
 {
   is_prime[0] = is_prime[1] = false;
   for (ll i = 2; i <= MAX_N; i++)
   {
     if (not is_prime[i]) continue;
+    primes.push_back(i);
     for (ll j = i * i; j <= MAX_N; j+=i)
       is_prime[j] = false;
   }
