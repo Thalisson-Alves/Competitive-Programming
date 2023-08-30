79d1ad1f585d4f5b14902b349368bba8b9848571
Thalisson-Alves <thalisson2030@gmail.com>
2023-08-30 20:36:52 -0300

    add LCA sparse table

---
+++ snips/lca_sparse_table.cpp
@@ -0,0 +1,33 @@
+struct LCA {
+  vector<int> height, euler, first;
+  DisjointSparseTable<pair<int, int>> table;
+ 
+  LCA(vector<vector<int>> &g, int root = 0) : height(g.size()), euler(g.size() * 2), first(g.size()), table(vector<pair<int, int>>(1)) {
+    vector<bool> visited(len(g));
+    dfs(g, root, visited);
+    int m = (int)euler.size();
+    vector<pair<int, int>> v(m);
+    for (int i = 0; i < m; i++) v[i] = {height[euler[i]], euler[i]};
+    table = DisjointSparseTable<pair<int, int>>(v, [](auto a, auto b) { return min(a, b); }, {INT_MAX, INT_MAX});
+  }
+ 
+  void dfs(vector<vector<int>> &adj, int node, vector<bool> &visited, int h = 0) {
+    visited[node] = true;
+    height[node] = h;
+    first[node] = len(euler);
+    euler.push_back(node);
+    for (auto to : adj[node]) {
+      if (!visited[to]) {
+        dfs(adj, to, visited, h + 1);
+        euler.push_back(node);
+      }
+    }
+  }
+
+  int lca(int u, int v) {
+    int left = first[u], right = first[v];
+    if (left > right) swap(left, right);
+    auto x = table.query(left, right);
+    return x.second;
+  }
+};
