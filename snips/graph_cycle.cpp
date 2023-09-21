7baacca73064f4a101ac985ee7eceb850556d10c
Thalisson-Alves <thalisson2030@gmail.com>
2023-09-21 14:35:19 -0300

    update graph cycle

--- snips/graph_cycle.cpp
+++ snips/graph_cycle.cpp
@@ -1,21 +1,36 @@
-bool has_cycle(const vector<vector<int>> &g, int s, vector<char> &vis, vector<char> &in_path, vector<int> *path = nullptr)
+void graph_cycles(const vector<vector<int>> &g, int u, int p, vector<int> &ps, vector<int> &color, int &cn, vector<vector<int>> &cycles)
 {
-  vis[s] = in_path[s] = 1;
-  if (path != nullptr)
-    path->push_back(s);
-  for (auto x : g[s])
-  {
-    if (!vis[x] && has_cycle(g, x, vis, in_path, path))
-      return true;
-    else if (in_path[x])
-    {
-      if (path != nullptr)
-        path->push_back(x);
-      return true;
-    }
+  if (color[u] == 2) {
+    return;
   }
-  in_path[s] = 0;
-  if (path != nullptr)
-    path->pop_back();
-  return false;
+
+  if (color[u] == 1) {
+    cn++;
+    int cur = p;
+    cycles.emplace_back();
+    auto &v = cycles.back();
+    v.push_back(cur);
+    while (cur != u) {
+      cur = ps[cur];
+      v.push_back(cur);
+    }
+    return;
+  }
+
+  ps[u] = p;
+  color[u] = 1;
+  for (auto v : g[u]) {
+    if (v == ps[u]) continue;
+    graph_cycles(g, v, u, ps, color, cn, cycles);
+  }
+
+  color[u] = 2;
+}
+
+vector<vector<int>> graph_cycles(const vector<vector<int>> &g, int u) {
+  vector<int> ps(g.size(), -1), color(g.size());
+  int cn = 0;
+  vector<vector<int>> cycles;
+  graph_cycles(g, u, -1, ps, color, cn, cycles);
+  return cycles;
 }
