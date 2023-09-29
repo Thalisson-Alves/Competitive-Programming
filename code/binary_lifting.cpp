61c9f99f79d1f3ba6aed4a688c697dacf6c627d9
Thalisson-Alves <thalisson2030@gmail.com>
2023-09-29 17:44:53 -0300

    add lca and arbitrary root

--- code/binary_lifting.cpp
+++ code/binary_lifting.cpp
@@ -1,25 +1,29 @@
-struct BinaryLifting
-{
+struct BinaryLifting {
   vector<int> far, level, parent;
 
-  BinaryLifting(vector<int> &ps) : far(ps.size(), -1), level(ps.size()), parent(ps)
-  {
-    level[0] = 1;
-    for (int i = 1; i < (int)parent.size(); i++)
-    {
-      level[i] = level[parent[i]] + 1;
+  BinaryLifting(const vector<vector<int>> &g, int root=0) : far(g.size(), -1), level(g.size()), parent(g.size()) {
+    level[root] = 1;
+    vector<int> q{root};
+    q.reserve(g.size());
+    for (int u = 0; u < (int)q.size(); u++) {
+      for (auto x : g[u]) if (x != parent[u]) {
+        parent[x] = u;
+        level[x] = level[u] + 1;
 
-      int p1 = parent[i];
-      int p2 = far[p1];
+        int p1 = u;
+        int p2 = far[u];
 
-      if (p2 > -1 and far[p2] > -1 and level[p1] - level[p2] == level[p2] - level[far[p2]])
-        far[i] = far[p2];
-      else
-        far[i] = p1;
+        if (p2 > -1 and far[p2] > -1 and level[p1] - level[p2] == level[p2] - level[far[p2]])
+          far[x] = far[p2];
+        else
+          far[x] = p1;
+
+        q.push_back(x);
+      }
     }
   }
 
-  int query(int node, int k) const
+  int kth_parent(int node, int k) const
   {
     while (node >= 0 and k > 0)
     {
@@ -36,5 +40,30 @@ struct BinaryLifting
     }
 
     return node;
+  }
+
+  int lca(int u, int v) const {
+    if (level[u] < level[v]) swap(u, v);
+
+    while (level[v] < level[u]) {
+      if (far[u] > -1 and level[v] <= level[far[u]])
+        u = far[u];
+      else
+        u = parent[u];
+    }
+
+    if (u == v) return u;
+
+    while (parent[u] != parent[v]) {
+      if (far[u] > -1 and far[v] > -1 and far[u] != far[v]) {
+        u = far[u];
+        v = far[v];
+      } else {
+        u = parent[u];
+        v = parent[v];
+      }
+    }
+
+    return parent[u];
   }
 };
