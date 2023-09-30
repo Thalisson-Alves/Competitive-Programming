c932903f0cb796868968734be132141b81992f2e
Thalisson-Alves <thalisson2030@gmail.com>
2023-09-30 11:50:07 -0300

    define operation as template parameter

--- code/segtree.cpp
+++ code/segtree.cpp
@@ -1,19 +1,20 @@
 #define F(expr) [](auto a, auto b) { return expr; }
-template <typename T> struct SegTree {
-  using Operation = T(*)(T, T);
+template <typename T, auto op>
+struct SegTree {
+  static_assert(std::is_convertible_v<decltype(op), std::function<T(T, T)>>,
+                "Operation must be convertible to std::function<T(T, T)>");
 
   int N;
+  const T identity = T();
   vector<T> ns;
-  Operation operation;
-  T identity;
 
-  SegTree(int n, Operation op = F(a+b), T neutral = T()) : N(n), ns(2 * N, neutral), operation(op), identity(neutral) {}
+  SegTree(int n, const T identity_ = T()) : N(n), identity(identity_), ns(2 * N, identity)  {}
 
-  SegTree(const vector<T> &v, Operation op = F(a+b), T neutral = T()) : SegTree((int)v.size(), op, neutral) {
+  SegTree(const vector<T> &v) : SegTree((int)v.size()) {
     copy(v.begin(), v.end(), ns.begin() + N);
 
     for (int i = N - 1; i > 0; --i)
-      ns[i] = operation(ns[2 * i], ns[2 * i + 1]);
+      ns[i] = op(ns[2 * i], ns[2 * i + 1]);
   }
 
   T query(size_t i) const {
@@ -21,35 +22,30 @@ template <typename T> struct SegTree {
   }
 
   T query(size_t l, size_t r) const {
-    auto a = l + N, b = r + N;
-    auto ans = identity;
-    // Non-associative operations needs to be processed backwards
-    stack<T> st;
-    while (a <= b) {
-      if (a & 1)
-        ans = operation(ans, ns[a++]);
-      if (not (b & 1))
-        st.push(ns[b--]);
+    l = l + N;
+    r = r + N;
 
-      a >>= 1;
-      b >>= 1;
+    auto ml = identity, mr = identity;
+    while (l <= r) {
+      if (l & 1) ml = op(ml, ns[l++]);
+      if (not (r & 1)) mr = op(ns[r--], mr);
+
+      l >>= 1;
+      r >>= 1;
     }
 
-    for (; !st.empty(); st.pop())
-      ans = operation(ans, st.top());
-
-    return ans;
+    return op(ml, mr);
   }
 
   void update(size_t i, T value) {
-    update_set(i, operation(ns[i + N], value));
+    set(i, op(ns[i + N], value));
   }
 
-  void update_set(size_t i, T value) {
+  void set(size_t i, T value) {
     auto a = i + N;
 
     ns[a] = value;
     while (a >>= 1)
-      ns[a] = operation(ns[2 * a], ns[2 * a + 1]);
+      ns[a] = op(ns[2 * a], ns[2 * a + 1]);
   }
 };
