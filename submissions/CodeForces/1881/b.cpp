ed139caa4a3bf2af61dd9a38c00dd28d45dcc9fc
Thalisson-Alves <thalisson2030@gmail.com>
2023-10-12 15:20:08 -0300

    math

---
+++ submissions/CodeForces/1881/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "Competitive-Programming/debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+const vector<pair<int, int>> dir4{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dir8{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+ll ops(ll n, ll k) {
+  return n / k - 1;
+}
+
+void solve() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+
+  ll d = gcd(gcd(a, b), c);
+  cout << (ops(a, d) + ops(b, d) + ops(c, d) <= 3 ? "YES" : "NO") << '\n';
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
