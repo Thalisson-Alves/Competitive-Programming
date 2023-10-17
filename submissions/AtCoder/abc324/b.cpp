94bade2e6972b1aab2510ed1740edf4a65488fb2
Thalisson-Alves <thalisson2030@gmail.com>
2023-10-17 08:13:35 -0300

    number theory, math

---
+++ submissions/AtCoder/abc324/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "Competitive-Programming/debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+const vector<pair<int, int>> dir4{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dir8{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+void solve() {
+  ll n;
+  cin >> n;
+
+  while (n%2 == 0)
+    n /= 2;
+  while (n%3 == 0)
+    n /= 3;
+
+  cout << (n == 1 ? "Yes" : "No") << '\n';
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
