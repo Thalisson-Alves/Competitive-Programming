855069317c85e7bc5fe3e33973fe6fca18e85947
Thalisson-Alves <thalisson2030@gmail.com>
2023-10-16 21:08:20 -0300

    update maxflow implementation

--- code/maxflow.cpp
+++ code/maxflow.cpp
@@ -1,65 +1,134 @@
-template <typename T, bool Undirected=false>
-struct MaxFlow {
-  vector<set<int>> adj;
-  vector<vector<T>> capacity;
+template <class Cap> struct MaxFlow {
+  public:
+    MaxFlow() : _n(0) {}
+    explicit MaxFlow(int n) : _n(n), g(n) {}
 
-  MaxFlow(int n) : adj(n), capacity(n, vector<T>(n)) {}
-  MaxFlow(const vector<set<int>> &g, const vector<vector<T>> &cap) : adj(g), capacity(cap) {}
-
-  void add(int u, int v, T w) {
-    adj[u].insert(v);
-    adj[v].insert(u);
-    capacity[u][v] += w;
-    if constexpr (Undirected)
-      capacity[v][u] += w;
-  }
-
-  T maxflow(int s, int t) {
-    T flow = T();
-    vector<int> parent(adj.size());
-    for (T new_flow; (new_flow = bfs(s, t, parent)); ) {
-      flow += new_flow;
-      for (int cur = t; cur != s; cur = parent[cur]) {
-        int prev = parent[cur];
-        capacity[prev][cur] -= new_flow;
-        capacity[cur][prev] += new_flow;
-      }
-    }
-    return flow;
-  }
-
-  vector<int> mincut(int s, int t) {
-    maxflow(s, t);
-    vector<int> cut(adj.size(), 1);
-    dfs(s, cut);
-    return cut;
-  }
-
-  void dfs(int s, vector<int> &cut) const {
-    cut[s] = 0;
-    for (auto next : adj[s]) if (cut[next] and capacity[s][next])
-      dfs(next, cut);
-  }
-
-  T bfs(int s, int t, vector<int> &parent) const {
-    fill(all(parent), -1);
-    parent[s] = -2;
-    queue<pair<int, T>> q;
-    q.emplace(s, numeric_limits<T>::max());
-
-    while (not q.empty()) {
-      auto [cur, flow] = q.front();
-      q.pop();
-
-      for (int next : adj[cur]) if (parent[next] == -1 and capacity[cur][next]) {
-        parent[next] = cur;
-        auto new_flow = min(flow, capacity[cur][next]);
-        if (next == t)
-          return new_flow;
-        q.emplace(next, new_flow);
-      }
+    int add_edge(int from, int to, Cap cap) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = int(pos.size());
+        pos.push_back({from, int(g[from].size())});
+        int from_id = int(g[from].size());
+        int to_id = int(g[to].size());
+        if (from == to) to_id++;
+        g[from].push_back(_Edge{to, to_id, cap});
+        g[to].push_back(_Edge{from, from_id, 0});
+        return m;
     }
 
-    return T();
-  }
+    struct Edge {
+        int from, to;
+        Cap cap, flow;
+    };
+
+    Edge get_edge(int i) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i].first][pos[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return Edge{pos[i].first, _e.to, _e.cap + _re.cap, _re.cap};
+    }
+    vector<Edge> edges() {
+        int m = int(pos.size());
+        vector<Edge> result;
+        for (int i = 0; i < m; i++) {
+            result.push_back(get_edge(i));
+        }
+        return result;
+    }
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto& _e = g[pos[i].first][pos[i].second];
+        auto& _re = g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) {
+        return flow(s, t, numeric_limits<Cap>::max());
+    }
+    Cap flow(int s, int t, Cap flow_limit) {
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+
+        vector<int> level(_n), iter(_n);
+        queue<int> que;
+
+        auto bfs = [&]() {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            while (que.size()) que.pop();
+            que.push(s);
+            while (!que.empty()) {
+                int v = que.front();
+                que.pop();
+                for (auto e : g[v]) {
+                    if (e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t) return;
+                    que.push(e.to);
+                }
+            }
+        };
+        auto dfs = [&](auto self, int v, Cap up) {
+            if (v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int& i = iter[v]; i < int(g[v].size()); i++) {
+                _Edge& e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d =
+                    self(self, e.to, min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up) return res;
+            }
+            level[v] = _n;
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            fill(iter.begin(), iter.end(), 0);
+            Cap f = dfs(dfs, t, flow_limit - flow);
+            if (!f) break;
+            flow += f;
+        }
+        return flow;
+    }
+
+    vector<bool> min_cut(int s) {
+        vector<bool> visited(_n);
+        queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int p = que.front();
+            que.pop();
+            visited[p] = true;
+            for (auto e : g[p]) {
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+  private:
+    int _n;
+    struct _Edge {
+        int to, rev;
+        Cap cap;
+    };
+    vector<pair<int, int>> pos;
+    vector<vector<_Edge>> g;
 };
