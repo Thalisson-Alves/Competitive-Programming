abcd074b83498d24d351495eeeac071918050853
Thalisson-Alves <thalisson2030@gmail.com>
2023-10-25 14:18:23 -0300

    rename array_cycle to successor_graph

--- code/successor_graph.cpp
+++ code/successor_graph.cpp
@@ -1,9 +1,9 @@
-struct ArrayCycle {
+struct SuccessorGraph {
   vector<vector<int>> paths;
   vector<int> path_num, pos;
   vector<char> is_cycle;
 
-  ArrayCycle(const vector<int> &v) : path_num(v.size()), pos(v.size()) {
+  SuccessorGraph(const vector<int> &v) : path_num(v.size()), pos(v.size()) {
     paths.reserve(v.size());
     is_cycle.reserve(v.size());
 
@@ -18,30 +18,28 @@ struct ArrayCycle {
         vis[cur] = 1;
       }
 
-      {
-        int cycle_start = 0;
-        for (; cycle_start < (int)path.size() and path[cycle_start] != cur; cycle_start++);
+      int cycle_start = 0;
+      for (; cycle_start < (int)path.size() and path[cycle_start] != cur; cycle_start++);
 
-        if (cycle_start > 0) {
-          paths.emplace_back();
-          for (int j = 0; j < cycle_start; j++) {
-            paths.back().push_back(path[j]);
-            pos[path[j]] = j;
-            path_num[path[j]] = (int)paths.size() - 1;
-          }
-          paths.back().push_back(cur);
-          is_cycle.push_back(false);
+      if (cycle_start > 0) {
+        paths.emplace_back();
+        for (int j = 0; j < cycle_start; j++) {
+          paths.back().push_back(path[j]);
+          pos[path[j]] = j;
+          path_num[path[j]] = (int)paths.size() - 1;
         }
+        paths.back().push_back(cur);
+        is_cycle.push_back(false);
+      }
 
-        if (cycle_start < (int)path.size()) {
-          paths.emplace_back();
-          for (int j = cycle_start; j < (int)path.size(); j++) {
-            paths.back().push_back(path[j]);
-            pos[path[j]] = j - cycle_start;
-            path_num[path[j]] = (int)paths.size() - 1;
-          }
-          is_cycle.push_back(true);
+      if (cycle_start < (int)path.size()) {
+        paths.emplace_back();
+        for (int j = cycle_start; j < (int)path.size(); j++) {
+          paths.back().push_back(path[j]);
+          pos[path[j]] = j - cycle_start;
+          path_num[path[j]] = (int)paths.size() - 1;
         }
+        is_cycle.push_back(true);
       }
     }
   }
@@ -74,6 +72,28 @@ struct ArrayCycle {
     return {cur, moves};
   }
 
+  // min cost to reach dest from cur
+  int reach(int cur, int dest) const {
+    int moves = 0;
+    while (not is_cycle[path_num[cur]] and path_num[cur] != path_num[dest]) {
+      auto &p = path(cur);
+      moves += (int)p.size() - pos[cur] - 1;
+      cur = p.back();
+    }
+
+    if (path_num[cur] != path_num[dest])
+      return -1;
+
+    if (pos[cur] <= pos[dest])
+      return moves + pos[dest] - pos[cur];
+
+    if (not is_cycle[path_num[cur]])
+      return -1;
+
+    return moves + pos[dest] + (int)path(cur).size() - pos[cur];
+  }
+
+private:
   void topological_order(const vector<int> &g, vector<char> &vis, vector<int> &order, int u)
   {
     vis[u] = true;
