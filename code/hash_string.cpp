54d48331537329037044be219edb7b4e8166f610
Thalisson-Alves <thalisson2030@gmail.com>
2023-10-30 07:20:24 -0300

    add max prefix/suffix

--- code/hash_string.cpp
+++ code/hash_string.cpp
@@ -16,6 +16,14 @@ struct HashString {
     }
   }
 
+  bool operator<(const HashString &a) const {
+    return hash() < a.hash();
+  }
+
+  pair<int, int> substr(int l) const {
+    return substr(l, size()-1);
+  }
+
   pair<int, int> substr(int l, int r) const {
     assert(0 <= l and l <= r and r < (int)suf.size() - 1);
     int a = (int)((((suf[l].first - (ll)suf[r+1].first * powers[r-l+1].first) % q1) + q1) % q1);
@@ -23,5 +31,33 @@ struct HashString {
     return {a, b};
   }
 
+  int max_prefix(const HashString &h) const {
+    int l = 0, r = (int)min(size(),h.size())-1;
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      if (substr(0, mid) == h.substr(0, mid)) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return l;
+  }
+
+  int max_suffix(const HashString &h) const {
+    int l = 1, r = (int)min(size(),h.size());
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      if (substr(size()-mid) == h.substr(h.size()-mid)) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return r;
+  }
+
   pair<int, int> hash() const { return suf[0]; }
+
+  int size() const { return (int)suf.size() - 1; }
 };
