94a3112616f684af18bc0de7f697b79ce3aa3c79
Thalisson-Alves <thalisson2030@gmail.com>
2023-11-04 12:12:14 -0300

    works with disconnected graphs

--- code/check_bipartite.cpp
+++ code/check_bipartite.cpp
@@ -1,24 +1,27 @@
 optional<vector<int>> check_bipartite(const vector<vector<int>> &g) {
   vector<int> color(g.size(), -1);
-  color[0] = 0;
+  for (int i = 0; i < (int)g.size(); i++) {
+    if (color[i] != -1) continue;
+    color[i] = 0;
 
-  queue<int> q;
-  q.push(0);
+    queue<int> q;
+    q.push(i);
 
-  while (not q.empty()) {
-    auto cur = q.front();
-    q.pop();
+    while (not q.empty()) {
+      auto cur = q.front();
+      q.pop();
 
-    for (auto x : g[cur]) {
-      if (color[x] == color[cur])
-        return {};
+      for (auto x : g[cur]) {
+        if (color[x] == color[cur])
+          return {};
 
-      if (color[x] == -1) {
-        color[x] = color[cur]^1;
-        q.push(x);
+        if (color[x] == -1) {
+          color[x] = color[cur]^1;
+          q.push(x);
+        }
       }
     }
   }
-
+  for (auto &x : color) if (x == -1) x = 0;
   return color;
 }
