b77c66a6a7eeed8151085078a8c6b58ed8bbd93c
Thalisson-Alves <thalisson2030@gmail.com>
2023-11-05 21:06:29 -0300

    add closest point to segment

--- code/2d.cpp
+++ code/2d.cpp
@@ -1,3 +1,9 @@
+constexpr double EPS = 1e-9;
+template <typename T> bool eq(const T a, const T b) {
+  if constexpr (is_floating_point_v<T>) return fabs(a - b) <= EPS;
+  else return a == b;
+}
+
 template<typename T=double> struct Point {
   T x, y;
 
@@ -33,6 +39,8 @@ template<typename T=double> struct Point {
   inline T dist2() const { return x * x + y * y; }
   inline double dist() const { return hypot(x, y); }
   inline double angle() const { return atan2(y, x); }
+  inline double norm() const { return sqrt(dot(*this)); }
+  inline Point rot90() const { return Point(-y, x); }
 };
 
 template<typename T> int orientation(const Point<T> &a, const Point<T> &b, const Point<T> &c) {
@@ -67,15 +75,15 @@ template<const bool include_collinear=false, typename T> bool ccw(const Point<T>
     return o > 0;
 }
 
-template<typename T=double> struct LineSegment {
+template<typename T=double> struct Segment {
   Point<T> a, b;
 
-  LineSegment() : a(), b() {}
-  LineSegment(Point<T> a_, Point<T> b_) : a(a_), b(b_) {}
+  Segment() : a(), b() {}
+  Segment(Point<T> a_, Point<T> b_) : a(a_), b(b_) {}
 
-  inline bool operator==(const LineSegment &l) const { return a == l.a and b == l.b; }
+  inline bool operator==(const Segment &l) const { return a == l.a and b == l.b; }
 
-  inline bool intersects(const LineSegment &l) const {
+  inline bool intersects(const Segment &l) const {
     if (eq(a.cross(b, l.a), (T)0) and eq(a.cross(b, l.b), (T)0)) {
       return min(a, b) <= max(l.a, l.b) and min(l.a, l.b) <= max(a, b);
     }
@@ -83,24 +91,27 @@ template<typename T=double> struct LineSegment {
   }
 
   inline bool intersects(const Point<T> &p) const {
-    return eq(a.cross(b, p), 0) and min(a, b) <= p and p <= max(a, b);
+    return eq(a.cross(b, p), (T)0) and min(a, b) <= p and p <= max(a, b);
   }
 
-  inline Point<T> intersection(const LineSegment &l) const {
+  inline Point<T> intersection(const Segment &l) const {
     assert(intersects(l));
-    if (eq(a.cross(b, l.a), 0) and eq(a.cross(b, l.b), 0)) {
+    if (eq(a.cross(b, l.a), (T)0) and eq(a.cross(b, l.b), (T)0)) {
       return min(a, b) <= max(l.a, l.b) ? max(a, l.a) : min(a, l.a);
     }
     return a + (b - a) * (l.a.cross(l.b, l.a) / l.a.cross(l.b, a - b));
   }
 
-  inline T dist2(const Point<T> &p) const {
-    if (eq(a.cross(b, p), 0)) return min((p - a).dist2(), (p - b).dist2());
-    return pow(a.cross(b, p), 2) / (a - b).dist2();
+  inline Point<T> closest(const Point<T> & p) const {
+    if ((p - a).dot(b - a) < EPS) return a;
+    if ((p - b).dot(a - b) < EPS) return b;
+
+    return a + (b - a) * ((p - a).dot(b - a) / (b - a).dist2());
   }
 
+  inline double dist2(const Point<T> &p) const { return (p-closest(p)).dist2(); }
   inline double dist(const Point<T> &p) const { return sqrt(dist2(p)); }
 
-  friend ostream& operator<<(ostream &os, const LineSegment &l) { return os << l.a << ' ' << l.b; }
-  friend istream& operator>>(istream &is, LineSegment &l) { return is >> l.a >> l.b; }
+  friend ostream& operator<<(ostream &os, const Segment &l) { return os << l.a << ' ' << l.b; }
+  friend istream& operator>>(istream &is, Segment &l) { return is >> l.a >> l.b; }
 };
