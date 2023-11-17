5e8a6085bd723f4b2976713c24533c95f27d21fa
Thalisson-Alves <thalisson2030@gmail.com>
2023-11-17 19:58:59 -0300

    adhoc

---
+++ submissions/AtCoder/abc193/b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "Competitive-Programming/debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+const vector<pair<int, int>> dir4{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dir8{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+void solve()
+{
+  int n;
+  cin >> n;
+  int res = INT_MAX;
+  for (int i = 0; i < n; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a < c)
+      res = min(res, b);
+  }
+  cout << (res == INT_MAX ? -1 : res) << '\n';
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
