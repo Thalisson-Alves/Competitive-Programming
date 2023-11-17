768f3bcb7eb71354150153a67b85a0c2719b6fe1
Thalisson-Alves <thalisson2030@gmail.com>
2023-11-17 19:59:14 -0300

    math, brute force

---
+++ submissions/AtCoder/abc193/c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "Competitive-Programming/debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+const vector<pair<int, int>> dir4{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dir8{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  set<ll> rep;
+  for (ll a = 2; a <= n / a; a++) {
+    for (ll x = a*a;; x*=a) {
+      rep.insert(x);
+      if (x > n / a) break;
+    }
+  }
+  cout << n - rep.size() << '\n';
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
