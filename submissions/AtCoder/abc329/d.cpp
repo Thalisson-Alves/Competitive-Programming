4af554c4fee623de1b858187fdc3032e67566018
Thalisson-Alves <thalisson2030@gmail.com>
2023-11-18 15:10:47 -0300

    implementation

---
+++ submissions/AtCoder/abc329/d.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "Competitive-Programming/debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+const vector<pair<int, int>> dir4{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dir8{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+void solve()
+{
+  int n, m;
+  cin >> n >> m;
+  int winner = -1, score = 0;
+  vector<int> hist(n);
+  for (int i = 0; i < m; i++) {
+    int x;
+    cin >> x;
+    hist[--x]++;
+    if (hist[x] > score or (hist[x] == score and x < winner))
+      winner = x, score = hist[x];
+    cout << winner+1 << '\n';
+  }
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
