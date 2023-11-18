7357c0cfff1fe0cc78670d1aef86db7e0974bfaf
Thalisson-Alves <thalisson2030@gmail.com>
2023-11-18 15:10:35 -0300

    brute force

---
+++ submissions/AtCoder/abc329/c.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "Competitive-Programming/debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+const vector<pair<int, int>> dir4{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dir8{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+void solve()
+{
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  ll res = 0;
+  for (int c = 'a'; c <= 'z'; c++) {
+    int cnt = 0, mx = 0;
+    for (int i = 0; i < n; i++) {
+      if (s[i] == c) cnt++;
+      else cnt = 0;
+      mx = max(mx, cnt);
+    }
+    res += mx;
+  }
+  cout << res << '\n';
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
