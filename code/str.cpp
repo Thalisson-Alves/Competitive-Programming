a5a0128922a3444ac089e494cc41736d08952527
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-04 12:12:09 -0300

    add split_into

--- code/str.cpp
+++ code/str.cpp
@@ -2,40 +2,27 @@ namespace str {
   constexpr string_view digits() { return "0123456789"; }
   constexpr string_view lower() { return "abcdefghijklmnopqrstuvwxyz"; }
   constexpr string_view upper() { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
-  template <typename T> inline string join(const T &values, const string_view &sep="") {
+  template <typename T> string join(const T &values, const string_view &sep=" ") {
     if (values.empty()) return {};
+    stringstream ss;
     auto it = values.begin();
-    string res = *it;
-    while (++it != values.end()) {
-      res += sep;
-      res += *it;
-    }
-    return res;
-  }
-  template <typename T> inline string join(const T &values, const char sep) {
-    return join(values, string_view(&sep, 1));
-  }
-  template <typename T> inline string join(const T &values, const char *sep) {
-    return join(values, string_view(sep));
+    ss << *it;
+    for (++it; it != values.end(); ++it) ss << sep << *it;
+    return ss.str();
   }
 
-  inline vector<string> split(const string &s, const string_view &sep=" ") {
+  vector<string> split(const string &s, const string_view &sep=" ") {
     vector<string> res;
     size_t pos = 0, last = 0;
     while ((pos = s.find(sep, last)) != string::npos) {
-      res.push_back(s.substr(last, pos - last));
+      if (pos > last)
+        res.push_back(s.substr(last, pos - last));
       last = pos + sep.size();
     }
     res.push_back(s.substr(last));
     return res;
   }
-  inline vector<string> split(const string &s, const char sep) {
-    return split(s, string_view(&sep, 1));
-  }
-  inline vector<string> split(const string &s, const char *sep) {
-    return split(s, string_view(sep));
-  }
-  template <typename T> inline vector<T> split(const string &s, const string_view &sep=" ") {
+  template <typename T> vector<T> split(const string &s, const string_view &sep=" ") {
     vector<T> res;
     for (const auto &x : split(s, sep)) {
       stringstream ss(x);
@@ -44,5 +31,15 @@ namespace str {
       res.emplace_back(val);
     }
     return res;
+  }
+  template <typename T> void split_into(const string &s, const string_view &sep, T out) {
+    stringstream ss(s);
+    string token;
+    while (getline(ss, token, sep[0])) {
+      stringstream ss2(token);
+      typename T::container_type::value_type val;
+      ss2 >> val;
+      *out++ = val;
+    }
   }
 };
