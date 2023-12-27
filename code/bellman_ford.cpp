59735936102d0dcd2830f27814b877a05f5cb57d
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-27 15:13:06 -0300

    update bellman ford to use Graph

--- code/bellman_ford.cpp
+++ code/bellman_ford.cpp
@@ -1,38 +1,41 @@
-bool bellman_ford(const vector<vector<pair<int, ll>>> &g, int s, vector<ll> &dist)
-{
-  int n = (int)g.size();
-  dist.assign(n, LLONG_MAX);
+// Returns: {dist, negative cycle}
+template <typename Graph> auto bellman_ford(const Graph &g, int s) {
+  using Weight = typename Graph::Weight;
 
-  vector<int> count(n);
-  vector<char> in_queue(n);
-  queue<int> q;
-
+  vector<Weight> dist(g.size(), numeric_limits<Weight>::max());
+  vector<int> count(g.size());
+  vector<bool> in_queue(g.size());
+  vector<int> prev(g.size(), -1);
+  queue<int> q({s});
   dist[s] = 0;
-  q.push(s);
-  in_queue[s] = true;
+  count[s] = 1;
 
-  while (not q.empty())
-  {
-    int cur = q.front();
-    q.pop();
-    in_queue[cur] = false;
+  while (not q.empty()) {
+    auto u = q.front(); q.pop();
+    in_queue[u] = false;
+    for (auto [v, w] : g.next(u)) {
+      if (dist[v] > dist[u] + w) {
+        dist[v] = dist[u] + w;
+        prev[v] = u;
+        if (not in_queue[v]) {
+          q.push(v);
+          in_queue[v] = true;
+          if (++count[v] > g.size()) {
+            for (int i = 0; i < g.size(); i++) v = prev[v];
 
-    for (auto [to, w] : g[cur])
-    {
-      if (dist[cur] + w < dist[to])
-      {
-        dist[to] = dist[cur] + w;
-        if (not in_queue[to])
-        {
-          q.push(to);
-          in_queue[to] = true;
-          count[to]++;
-          if (count[to] > n)
-            return false;
+            vector<int> cycle;
+            for (int x = v; x != -1; x = prev[x]) {
+              cycle.push_back(x);
+              if (x == v and cycle.size() > 1)
+                break;
+            }
+            reverse(all(cycle));
+            return make_pair(dist, cycle);
+          }
         }
       }
     }
   }
 
-  return true;
+  return make_pair(dist, vector<int>());
 }
