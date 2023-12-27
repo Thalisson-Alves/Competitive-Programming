9c68c45f07bbc6bb012aa5e0d14944e1f1df273e
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-27 15:11:25 -0300

    brute force, graph, bitset

---
+++ submissions/CSES/reachable-nodes.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+/*
+Used to define recursive lambdas,  first argument is the function itself
+
+auto value = y_combinator([](auto &&gcd, int a, int b) -> int {
+  return b ? gcd(b, a % b) : a;
+});
+*/
+template <class Fun> class y_combinator_result {
+  Fun fun_;
+
+  public:
+  template <class T>
+  explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+  template <class... Args> decltype(auto) operator()(Args &&...args) {
+    return fun_(std::ref(*this), std::forward<Args>(args)...);
+  }
+};
+
+template <class Fun> decltype(auto) y_combinator(Fun &&fun) {
+  return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> g(n);
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    --u, --v;
+    g[u].push_back(v);
+  }
+
+  vector<bitset<50'000>> reach(n);
+  auto f = y_combinator([&](auto &&dfs, int u) -> int {
+    if (reach[u].any()) return (int)reach[u].count();
+
+    for (auto v : g[u]) {
+      dfs(v);
+      reach[u] |= reach[v];
+    }
+    reach[u].set(u);
+    return (int)reach[u].count();
+  });
+
+  for (int i = 0; i < n; i++) {
+    cout << f(i) << " \n"[i == n-1];
+  }
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
