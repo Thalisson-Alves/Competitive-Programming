e88f08e5f903b6f06a36052b358b2653df05aa33
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-27 15:12:44 -0300

    reconstruct path

---
+++ code/reconstruct_path.cpp
@@ -0,0 +1,10 @@
+template <typename Node> vector<Node> reconstruct_path(Node t, const vector<Node> &prev, Node default_node = -1) {
+  vector<Node> path;
+  if (prev[t] == -1)
+    return {};
+
+  for (; t != default_node; t = prev[t])
+    path.push_back(t);
+  reverse(all(path));
+  return path;
+}
