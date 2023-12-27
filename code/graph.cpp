cbab7ab2d75e86a9fd10b7e3d0698fe043b37a13
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-27 15:12:25 -0300

    graph

---
+++ code/graph.cpp
@@ -0,0 +1,69 @@
+template <typename T, bool directed> struct Graph {
+  using Weight = T;
+  struct Edge {
+    int from, to;
+    T weight;
+  };
+
+  vector<vector<int>> g;
+  vector<Edge> edges;
+
+  Graph(int n) : g(n) {}
+
+  int add_edge(int from, int to, T cost = 1) {
+    assert(0 <= from and from < size() and 0 <= to and to < size());
+    int id = (int)edges.size();
+    g[from].push_back(id);
+    if constexpr (not directed)
+      g[to].push_back(id);
+    edges.push_back({from, to, cost});
+    return id;
+  }
+
+  struct NodeIterator {
+    const Graph &g; int cur;
+
+    NodeIterator(const Graph &g_, int cur_ = 0) : g(g_), cur(cur_) {}
+    int operator*() const { return cur; }
+    bool operator!=(const NodeIterator &rhs) const { return cur != rhs.cur; }
+    void operator++() { cur++; }
+    void operator++(int) { cur++; }
+
+    NodeIterator begin() const { return *this; }
+    NodeIterator end() const { return NodeIterator(g, g.size()); }
+  };
+  NodeIterator nodes() const { return NodeIterator(*this); }
+  NodeIterator begin() const { return nodes(); }
+  NodeIterator end() const { return nodes().end(); }
+
+  struct EdgeIterator {
+    const Graph &g;
+    int node;
+    int cur;
+
+    EdgeIterator(const Graph &g_, int node_, int cur_ = 0) : g(g_), node(node_), cur(cur_) {}
+    pair<int, Weight> operator*() const {
+      auto e = g.edges[g.g[node][cur]];
+      return {e.to ^ e.from ^ node, e.weight};
+    }
+    bool operator!=(const EdgeIterator &rhs) const { return cur != rhs.cur; }
+    void operator++() { cur++; }
+    void operator++(int) { cur++; }
+
+    EdgeIterator begin() const { return *this; }
+    EdgeIterator end() const { return EdgeIterator(g, node, (int)g.g[node].size()); }
+  };
+  EdgeIterator next(int node) const { return EdgeIterator(*this, node); }
+
+  int size() const { return static_cast<int>(g.size()); }
+  constexpr bool is_directed() const { return directed; }
+
+  Graph reverse() const {
+    Graph rev(size());
+    for (const auto &e : edges)
+      rev.add_edge(e.to, e.from, e.weight);
+    return rev;
+  }
+};
+template <typename T> using Digraph = Graph<T, true>;
+template <typename T> using Undigraph = Graph<T, false>;
