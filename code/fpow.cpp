63f8e160abc5955677b9758cbc83b219848db6c5
Thalisson-Alves <thalisson2030@gmail.com>
2024-01-07 17:20:22 -0300

    iterative fpow

--- code/fpow.cpp
+++ code/fpow.cpp
@@ -1,17 +1,19 @@
-ll fpow(ll x, ll p)
-{
-  if (p == 0) return 1;
-  auto ans = fpow(x, p >> 1);
-  ans *= ans;
-  if (p & 1) ans *= x;
-  return ans;
+ll fpow(ll x, ll p) {
+  ll res = 1;
+  while (p) {
+    if (p & 1) res *= x;
+    p >>= 1;
+    x *= x;
+  }
+  return res;
 }
 
-ll fpow(ll x, ll p, ll mod)
-{
-  if (p == 0) return 1;
-  auto ans = fpow(x, p >> 1, mod);
-  ans = ans * ans % mod;
-  if (p & 1) ans = ans * x % mod;
-  return ans;
+ll fpow(ll x, ll p, ll mod) {
+  ll res = 1;
+  while (p) {
+    if (p & 1) res = res * x % mod;
+    p >>= 1;
+    x = x * x % mod;
+  }
+  return res;
 }
