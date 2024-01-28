ce31f16340db1a7573465c230932e4d151b3df51
Thalisson-Alves <thalisson2030@gmail.com>
2024-01-28 14:33:40 -0300

    graph, lumberjack, greedy

--- submissions/Gym/102346/d.cpp
+++ submissions/Gym/102346/d.cpp
@@ -11,73 +11,34 @@ using namespace std;
 
 using ll = long long;
 
-/*
-Used to define recursive lambdas,  first argument is the function itself
-
-auto value = y_combinator([](auto &&gcd, int a, int b) -> int {
-  return b ? gcd(b, a % b) : a;
-});
-*/
-template <class Fun> class y_combinator_result {
-  Fun fun_;
-
-  public:
-  template <class T>
-  explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
-
-  template <class... Args> decltype(auto) operator()(Args &&...args) {
-    return fun_(std::ref(*this), std::forward<Args>(args)...);
-  }
-};
-
-template <class Fun> decltype(auto) y_combinator(Fun &&fun) {
-  return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
-}
-
 void solve() {
   int n, k;
   cin >> n >> k;
-
-  vector<vector<int>> g(n);
+  vector<int> ps(n, -1), in(n);
   for (int i = 1; i < n; i++) {
     int x;
     cin >> x;
-    g[x-1].push_back(i);
+    ps[i] = --x;
+    ++in[x];
   }
 
-  vector<int> height(n, 1);
-  y_combinator([&](auto &&dfs, int u) -> void {
-    for (auto x : g[u]) {
-      dfs(x);
-      height[u] = max(height[u], height[x] + 1);
-    }
-  })(0);
+  queue<pair<int, int>> q;
+  for (int i = 0; i < n; i++)
+    if (!in[i])
+      q.emplace(i, 1);
 
-  int res = 0;
-  auto cmp = [&height](int a, int b) { return height[a] < height[b]; };
-  priority_queue<int, vector<int>, decltype(cmp)> roots(cmp);
-  roots.push(0);
-  while (k-- and not roots.empty()) {
-    auto u = roots.top();
-    roots.pop();
+  vector<int> paths;
+  while (!q.empty()) {
+    auto [u, d] = q.front();
+    q.pop();
 
-    do {
-      ++res;
-      int mx = -1;
-      for (auto x : g[u]) {
-        if (mx == -1 or height[x] > height[mx]) {
-          if (~mx) roots.push(mx);
-          mx = x;
-        } else {
-          roots.push(x);
-        }
-      }
-
-      u = mx;
-    } while (~u);
+    if (!u or --in[ps[u]])
+      paths.push_back(d);
+    else
+      q.emplace(ps[u], d + 1);
   }
 
-  cout << res << '\n';
+  cout << accumulate(paths.rbegin(), paths.rbegin() + k, 0) << '\n';
 }
 
 int32_t main() {
