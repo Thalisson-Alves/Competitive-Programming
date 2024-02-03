75e24b850c318f5ab1db82893b59e859b96b5643
Thalisson-Alves <thalisson2030@gmail.com>
2024-02-03 20:22:13 -0300

    add utility functions

--- code/graphs/2_sat.cpp
+++ code/graphs/2_sat.cpp
@@ -1,30 +1,35 @@
-struct Sat2
-{
+struct Sat2 {
   vector<vector<int>> g, gr;
 
   Sat2(int variables) : g(variables << 1), gr(variables << 1) {}
 
-  void add_clause(int x, bool sign_x, int y, bool sign_y)
-  {
-    g[var(x, not sign_x)].push_back(var(y, sign_y));
-    g[var(y, not sign_y)].push_back(var(x, sign_x));
-
-    gr[var(y, sign_y)].push_back(var(x, not sign_x));
-    gr[var(x, sign_x)].push_back(var(y, not sign_y));
+  void add_implication(int x, bool sign_x, int y, bool sign_y) {
+    g[var(x, sign_x)].push_back(var(y, sign_y));
+    gr[var(y, sign_y)].push_back(var(x, sign_x));
+  }
+  void add_xor(int x, bool sign_x, int y, bool sign_y) {
+    add_or(x, sign_x, y, sign_y);
+    add_or(x, not sign_x, y, not sign_y);
+  }
+  void add_and(int x, bool sign_x, int y, bool sign_y) {
+    add_or(x, sign_x, x, sign_x);
+    add_or(y, sign_y, y, sign_y);
+  }
+  void add_or(int x, bool sign_x, int y, bool sign_y) {
+    add_implication(x, not sign_x, y, sign_y);
+    add_implication(y, not sign_y, x, sign_x);
   }
 
   inline int var(int x, bool sign) const { return (x << 1) + sign; }
   inline int var_neg(int v) const { return v ^ 1; }
   inline bool var_sign(int v) const { return v & 1; }
 
-  vector<bool> solve()
-  {
+  vector<bool> solve() {
     auto order = topological_order(g);
     auto components = mark_components(gr, order);
     vector<int> comp_order(components.size(), (int)g.size());
     vector<bool> ans(g.size() >> 1);
-    for (int i = 0; i < (int)g.size(); i++)
-    {
+    for (int i = 0; i < (int)g.size(); i++) {
       auto a = order[i];
       auto b = var_neg(order[i]);
 
