fa0e0740f2c5133d66997195b51459d0caff1149
Thalisson-Alves <thalisson2030@gmail.com>
2024-02-04 17:41:52 -0300

    factor out check_bipartite

--- submissions/Gym/101462/d.cpp
+++ submissions/Gym/101462/d.cpp
@@ -154,6 +154,36 @@ private:
   }
 };
 
+template <typename Graph> bool check_bipartite(const Graph &g, int u, vector<int> &color, const set<int> &can) {
+  assert((int)color.size() >= g.size());
+
+  color[u] = 0;
+  queue<int> q;
+  q.push(u);
+  while (not q.empty()) {
+    auto cur = q.front();
+    q.pop();
+
+    for (auto [x, _] : g[cur]) if (can.count(x)) {
+      if (color[x] == color[cur]) return false;
+      if (color[x] == -1) {
+        color[x] = color[cur]^1;
+        q.push(x);
+      }
+    }
+  }
+  return true;
+}
+template <typename Graph> vector<int> check_bipartite(const Graph &g) {
+  vector<int> color(g.size(), -1);
+  for (int i = 0; i < g.size(); i++) {
+    if (color[i] != -1) continue;
+    if (!check_bipartite(g, i, color)) return {};
+  }
+  for (auto &x : color) if (x == -1) x = 0;
+  return color;
+}
+
 void solve()
 {
   int n, m;
@@ -169,34 +199,11 @@ void solve()
   set<int> res;
   vector<int> color(n, -1);
   for (int i = 0; i < (int)bt.blocks.size(); i++) {
-    set<int> can(all(bt.blocks[i]));
+    set<int> can;
     for (auto x : bt.blocks[i])
-      color[x] = -1;
+      color[x] = -1, can.insert(x);
 
-    bool bipartite = true;
-
-    queue<int> q;
-    q.push(bt.blocks[i][0]);
-    color[bt.blocks[i][0]] = 0;
-
-    while (not q.empty() and bipartite) {
-      auto cur = q.front();
-      q.pop();
-
-      for (auto [x, _] : g[cur]) if (can.count(x)) {
-        if (color[x] == color[cur]) {
-          bipartite = false;
-          break;
-        }
-
-        if (color[x] == -1) {
-          color[x] = color[cur]^1;
-          q.push(x);
-        }
-      }
-    }
-
-    if (not bipartite)
+    if (!check_bipartite(g, bt.blocks[i][0], color, can))
       res.insert(all(can));
   }
   cout << res.size() << '\n';
