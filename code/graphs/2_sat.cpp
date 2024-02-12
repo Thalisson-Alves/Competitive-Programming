258a5ddbadd5aa41f941f84c61d0b64870523b94
Thalisson-Alves <thalisson2030@gmail.com>
2024-02-12 17:17:23 -0300

    merge topological order

--- code/graphs/2_sat.cpp
+++ code/graphs/2_sat.cpp
@@ -44,4 +44,45 @@ struct Sat2 {
     }
     return ans;
   }
+private:
+  static vector<int> topological_order(const vector<vector<int>> &g) {
+    vector<char> vis(g.size());
+    vector<int> order;
+    order.reserve(g.size());
+    auto dfs = [&](auto &&self, int u) -> void {
+      vis[u] = true;
+      for (auto v : g[u])
+        if (not vis[v])
+          self(self, v);
+      order.push_back(u);
+    };
+
+    for (auto i = 0; i < (int)g.size(); i++)
+      if (not vis[i])
+        dfs(dfs, i);
+
+    reverse(order.begin(), order.end());
+    return order;
+  }
+
+  static vector<int> mark_components(const vector<vector<int>> &gr, const vector<int> &order) {
+    vector<int> comps(gr.size(), -1);
+    auto dfs = [&](auto &&self, int u) -> void {
+      for (auto x : gr[u]) {
+        if (comps[x] == -1) {
+          comps[x] = comps[u];
+          self(self, x);
+        }
+      }
+    };
+
+    int id = 0;
+    for (auto u : order) {
+      if (comps[u] == -1) {
+        comps[u] = id++;
+        dfs(dfs, u);
+      }
+    }
+    return comps;
+  }
 };
