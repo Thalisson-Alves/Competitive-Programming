0c10235784b05845feeb56e61dde45d4b73c0cf1
Thalisson-Alves <thalisson2030@gmail.com>
2024-02-25 10:58:26 -0300

    implement add_segment

--- code/geometry/lichao_tree.cpp
+++ code/geometry/lichao_tree.cpp
@@ -1,3 +1,16 @@
+/* LiChaoTree
+ *
+ * Add lines (ax + b) and query maximum value at `x`.
+ * If you want to get minimum value, set inf = numeric_limits<T>::max().
+ * In case of overflow, try to compress `x` values.
+ *
+ * - query(x) -> get maximum `y` value at `x`
+ *   - O(log (HI-LO))
+ * - add(Line s) -> add line `s` to the tree
+ *   - O(log (HI-LO))
+ * - add_segment(Line s, l, r) -> add line `s` to the tree in range [l, r]
+ *   - O(log^2 (HI-LO))
+ */
 template <typename T = ll, T LO = T(-1e9), T HI = T(1e9)> struct LiChaoTree {
   // get max value at x by default
   // to get min value, set inf = numeric_limits<T>::max()
@@ -14,15 +27,16 @@ template <typename T = ll, T LO = T(-1e9), T HI = T(1e9)> struct LiChaoTree {
     T a, b;
     array<int, 2> ch;
     Line(T a_ = 0, T b_ = inf) : a(a_), b(b_), ch({-1, -1}) { }
-    T eval(T x) { return a * x + b; }
+    constexpr T eval(T x) const { return a * x + b; }
+    constexpr bool is_leaf() const { return ch[0] == -1 and ch[1] == -1; }
   };
   vector<Line> ln;
   LiChaoTree() { ln.emplace_back(); }
 
   T query(T x, int v = 0, T l = LO, T r = HI) {
     auto m = l + (r - l) / 2, val = ln[v].eval(x);
-    if (val == inf) return val;
-    if (x < m) return best(val, query(x, ch(v, 0), l, m-1));
+    if (ln[v].is_leaf()) return val;
+    if (x <= m) return best(val, query(x, ch(v, 0), l, m));
     else return best(val, query(x, ch(v, 1), m+1, r));
   }
 
@@ -33,8 +47,15 @@ template <typename T = ll, T LO = T(-1e9), T HI = T(1e9)> struct LiChaoTree {
     bool R = compare(s.eval(r), ln[v].eval(r));
     if (M) swap(ln[v], s), swap(ln[v].ch, s.ch);
     if (s.b == inf) return;
-    if (L != M) add(s, ch(v, 0), l, m-1);
+    if (L != M) add(s, ch(v, 0), l, m);
     else if (R != M) add(s, ch(v, 1), m+1, r);
+  }
+
+  void add_segment(Line s, T l, T r, int v = 0, T L = LO, T R = HI) {
+    if (l <= L and R <= r) return add(s, v, L, R);
+    auto m = L + (R - L) / 2;
+    if (l <= m) add_segment(s, l, r, ch(v, 0), L, m);
+    if (r > m) add_segment(s, l, r, ch(v, 1), m+1, R);
   }
 private:
   int ch(int v, bool b) {
