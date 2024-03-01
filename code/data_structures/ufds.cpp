3b99169fd40bc555051edab66eaf2e06e0f6f070
Thalisson-Alves <thalisson2030@gmail.com>
2024-03-01 11:54:39 -0300

    rename methods

--- code/data_structures/ufds.cpp
+++ code/data_structures/ufds.cpp
@@ -2,35 +2,26 @@ struct UFDS {
   vector<int> ps, sz;
   int components;
 
-  UFDS(int n) : ps(n), sz(n, 1), components(n)
-  {
+  UFDS(int n) : ps(n), sz(n, 1), components(n) {
     iota(all(ps), 0);
   }
 
-  int find_set(int x)
-  {
-    return (x == ps[x] ? x : (ps[x] = find_set(ps[x])));
+  int find(int x) {
+    return (x == ps[x] ? x : (ps[x] = find(ps[x])));
   }
 
-  bool same_set(int x, int y)
-  {
-    return find_set(x) == find_set(y);
+  bool same(int x, int y) {
+    return find(x) == find(y);
   }
 
-  bool union_set(int x, int y)
-  {
-    x = find_set(x);
-    y = find_set(y);
-
+  bool join(int x, int y) {
+    x = find(x);
+    y = find(y);
     if (x == y) return false;
-
-    if (sz[x] < sz[y])
-      swap(x, y);
-
+    if (sz[x] < sz[y]) swap(x, y);
     ps[y] = x;
     sz[x] += sz[y];
-
-    components--;
+    --components;
     return true;
   }
 };
