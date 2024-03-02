26e08bbff9e1d202ba99e0195c481bfb682786ad
Thalisson-Alves <thalisson2030@gmail.com>
2024-03-02 13:46:53 -0300

    add ordered_multiset

--- code/data_structures/ordered_set.cpp
+++ code/data_structures/ordered_set.cpp
@@ -2,5 +2,5 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
