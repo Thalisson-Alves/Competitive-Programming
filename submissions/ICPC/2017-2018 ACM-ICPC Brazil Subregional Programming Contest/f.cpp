3fa8bad7a9732ee831caa095f9d02573edc22ed1
Thalisson-Alves <thalisson2030@gmail.com>
2024-03-30 22:21:05 -0300

    sorting, greedy

---
+++ submissions/ICPC/2017-2018 ACM-ICPC Brazil Subregional Programming Contest/f.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  map<int, int> mp;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    mp[-x]++;
+  }
+  int res = 0;
+  for (auto [_, x] : mp) {
+    res += x;
+    if (res >= k) {
+      break;
+    }
+  }
+  cout << res << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
