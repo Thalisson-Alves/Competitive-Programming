df24275c080a49bb40e0e4973f3f5eff854c7a4b
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-06 08:27:47 -0300

    adhoc

---
+++ submissions/Gym/103960/i.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  char res = 'S';
+  for (int x; cin >> x; (x == 9 ? res = 'F' : 0));
+  cout << res << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
