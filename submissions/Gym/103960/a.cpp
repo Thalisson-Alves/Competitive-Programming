11be6448f0b945e5ec69d26f8b57f6ef58701b5b
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-06 08:30:04 -0300

    string, adhoc

---
+++ submissions/Gym/103960/a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  s += 'b';
+  int cnt = 0, res = 0;
+  for (auto c : s) {
+    if (c == 'a') {
+      ++cnt;
+    } else {
+      if (cnt > 1) res += cnt;
+      cnt = 0;
+    }
+  }
+  cout << res << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
