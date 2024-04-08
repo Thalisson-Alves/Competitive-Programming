730fe7ce01aaacbe94d8f76f5e855851c6778319
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-08 10:07:43 -0300

    single function

--- code/graphs/tree_flatten.cpp
+++ code/graphs/tree_flatten.cpp
@@ -1,18 +1,17 @@
-void tree_flatten(const vector<vector<int>> &g, int u, int p, vector<int> &pre, vector<pair<int,int>> &pos, int &idx) {
-  ++idx;
-  pre.push_back(u);
-  for (auto x : g[u])
-    if (x != p)
-      tree_flatten(g, x, u, pre, pos, idx);
-  pos[u].second = idx;
-}
-
-vector<pair<int, int>> tree_flatten(const vector<vector<int>> &g, int root=0) {
+vector<pair<int, int>> tree_flatten(const vector<vector<int>> &g, int root = 0) {
   vector<int> pre;
   pre.reserve(g.size());
   vector<pair<int, int>> flat(g.size());
   int timer = -1;
-  tree_flatten(g, root, -1, pre, flat, timer);
+  auto dfs = [&](auto &&self, int u, int p) -> void {
+    ++timer;
+    pre.push_back(u);
+    for (auto v : g[u]) if (v != p) {
+      self(self, v, u);
+    }
+    flat[u].second = timer;
+  };
+  dfs(dfs, root, -1);
   for (int i = 0; i < (int)g.size(); i++)
     flat[pre[i]].first = i;
   return flat;
