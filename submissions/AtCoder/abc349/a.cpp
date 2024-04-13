13728d628b9a64fb3c2aebe447384cd179dfedc4
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-13 11:41:32 -0300

    adhoc

---
+++ submissions/AtCoder/abc349/a.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int n;
+  cin >> n;
+  int res = 0;
+  for (int i = 0; i < n - 1; i++) {
+    int x;
+    cin >> x;
+    res -= x;
+  }
+  cout << res << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
