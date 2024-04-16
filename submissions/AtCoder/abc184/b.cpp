f9c2dd7951b7244450f3c9d4b5bcb25c32f2ed62
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-16 08:45:56 -0300

    implementation

---
+++ submissions/AtCoder/abc184/b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  string s;
+  cin >> s;
+  for (auto c : s) {
+    if (c == 'o') ++m;
+    else m = max(m - 1, 0);
+  }
+  cout << m << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
