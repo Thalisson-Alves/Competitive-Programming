bb63ed2979a8e009b224db642796150386a5086f
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-17 20:21:47 -0300

    math

---
+++ submissions/AtCoder/abc189/b.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  ll res = 0;
+  for (int i = 0; i < n; i++) {
+    int v, p;
+    cin >> v >> p;
+    res += v * p;
+    if (res > k * 100) {
+      cout << i + 1 << '\n';
+      return;
+    }
+  }
+  cout << "-1\n";
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
