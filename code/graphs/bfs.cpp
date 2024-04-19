03c6a659eb0472e209e6aa233c8995c1f91339db
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-19 11:47:18 -0300

    update graph usage

--- code/graphs/bfs.cpp
+++ code/graphs/bfs.cpp
@@ -1,21 +1,17 @@
-// Returns: {dist, prev}
 template <typename Graph> auto bfs(const Graph &g, int s) {
-  using Weight = typename Graph::Weight;
-  vector<Weight> dist(g.size(), numeric_limits<Weight>::max());
-  vector<int> prev(g.size(), -1);
+  vector<int> dist(g.size(), numeric_limits<int>::max());
   dist[s] = 0;
   queue<int> q;
   q.push(s);
   while (not q.empty()) {
     auto u = q.front(); q.pop();
-    for (auto [v, w] : g.next(u)) {
-      if (dist[v] > dist[u] + w) {
-        dist[v] = dist[u] + w;
-        prev[v] = u;
+    for (auto v : g[u]) {
+      if (dist[v] > dist[u] + 1) {
+        dist[v] = dist[u] + 1;
         q.push(v);
       }
     }
   }
 
-  return make_pair(dist, prev);
+  return dist;
 }
