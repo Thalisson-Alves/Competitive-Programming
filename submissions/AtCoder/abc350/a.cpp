53aead9f09f41aeab0f3c525ca6f9985bc05caba
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-20 11:50:22 -0300

    adhoc

---
+++ submissions/AtCoder/abc350/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  string s;
+  cin >> s;
+  auto x = stoi(s.substr(3));
+  cout << (x >= 1 and x < 350 and x != 316 ? "Yes" : "No") << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
