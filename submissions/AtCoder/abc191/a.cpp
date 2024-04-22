a30847ab3180e4e351252c2e646754e711e6092d
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-22 20:10:06 -0300

    adhoc

---
+++ submissions/AtCoder/abc191/a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int v, t, s, d;
+  cin >> v >> t >> s >> d;
+  cout << (v * t <= d and d <= v * s ? "No" : "Yes") << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
