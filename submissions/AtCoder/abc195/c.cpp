c5211e0c9bf997056894f3235101357ebbcb5495
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-24 13:24:02 -0300

    math, implementation

---
+++ submissions/AtCoder/abc195/c.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  ll n;
+  cin >> n;
+  ll res = 0;
+  for (ll base = 1000; base <= n; base *= 1000)
+    res += n - base + 1;
+  cout << res << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
