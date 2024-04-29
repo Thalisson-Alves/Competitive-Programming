b030cd522c401df6ce8c7dd2035e6480e419fc3f
Thalisson-Alves <thalisson2030@gmail.com>
2024-04-29 12:11:49 -0300

    adhoc, strings

---
+++ submissions/AtCoder/abc197/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  char a, b, c;
+  cin >> a >> b >> c;
+  cout << b << c << a << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
