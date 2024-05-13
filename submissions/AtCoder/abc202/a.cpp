ec0217fdcb77a11743c7fc8957976b2ab755fc77
Thalisson-Alves <thalisson2030@gmail.com>
2024-05-13 13:01:07 -0300

    adhoc math

---
+++ submissions/AtCoder/abc202/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << 7*3 - (a + b + c) << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
