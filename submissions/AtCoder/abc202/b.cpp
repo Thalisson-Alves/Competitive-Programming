47c9cb14796ed8799e3d5e73ea640b456b4de444
Thalisson-Alves <thalisson2030@gmail.com>
2024-05-13 13:01:15 -0300

    adhoc, strings

---
+++ submissions/AtCoder/abc202/b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  string s;
+  cin >> s;
+  reverse(s.begin(), s.end());
+  for (auto &c : s) {
+    if (c == '6') c = '9';
+    else if (c == '9') c = '6';
+  }
+  cout << s << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
