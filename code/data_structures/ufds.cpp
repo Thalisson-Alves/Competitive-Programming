875819891b0186df14f95792c098ccaf50937eef
Thalisson-Alves <thalisson2030@gmail.com>
2024-05-15 10:39:14 -0300

    add function to create new roots

--- code/data_structures/ufds.cpp
+++ code/data_structures/ufds.cpp
@@ -2,17 +2,13 @@ struct UFDS {
   vector<int> ps, sz;
   int components;
 
-  UFDS(int n) : ps(n), sz(n, 1), components(n) {
+  UFDS(int n = 0) : ps(n), sz(n, 1), components(n) {
     iota(ps.begin(), ps.end(), 0);
   }
 
-  int find(int x) {
-    return (x == ps[x] ? x : (ps[x] = find(ps[x])));
-  }
+  int find(int x) { return (x == ps[x] ? x : (ps[x] = find(ps[x]))); }
 
-  bool same(int x, int y) {
-    return find(x) == find(y);
-  }
+  bool same(int x, int y) { return find(x) == find(y); }
 
   bool join(int x, int y) {
     x = find(x);
@@ -24,4 +20,12 @@ struct UFDS {
     --components;
     return true;
   }
+
+  int add() {
+    int id = (int)ps.size();
+    ps.push_back(id);
+    sz.push_back(1);
+    ++components;
+    return id;
+  };
 };
