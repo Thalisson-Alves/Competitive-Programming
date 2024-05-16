109b2d17367c3032e48679e4046299f6b24e558c
Thalisson-Alves <thalisson2030@gmail.com>
2024-05-16 09:03:51 -0300

    adhoc

---
+++ submissions/ICPC/2014-2015 ACM-ICPC Brazil Subregional Programming Contest/c.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  cout << max(a, b) << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
