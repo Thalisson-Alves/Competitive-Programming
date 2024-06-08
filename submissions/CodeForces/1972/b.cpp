41f028dccf3c93f793377e7abff3425c8c0b8ac0
Thalisson-Alves <thalisson2030@gmail.com>
2024-06-08 13:29:10 -0300

    adhoc

---
+++ submissions/CodeForces/1972/b.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  cout << (count(s.begin(), s.end(), 'U')&1 ? "YES" : "NO") << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+
+  return 0;
+}
