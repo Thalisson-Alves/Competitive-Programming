4dd2e488c4a871cc936952b1543ad9508fe550f1
Thalisson-Alves <thalisson2030@gmail.com>
2024-06-21 21:04:35 -0300

    add sum in range

--- code/data_structures/wavelet_tree.cpp
+++ code/data_structures/wavelet_tree.cpp
@@ -2,96 +2,76 @@ template <typename T> struct WaveletTree {
   struct Node {
     T lo, hi;
     int left_child, right_child;
-    vector<int> b;
+    vector<int> pcnt;
+    vector<ll> psum;
 
-    Node(int lo_, int hi_) : lo(lo_), hi(hi_), left_child(0), right_child(0), b() {}
+    Node(int lo_, int hi_) : lo(lo_), hi(hi_), left_child(0), right_child(0), pcnt(), psum() {}
   };
 
   vector<Node> nodes;
   WaveletTree(vector<T> v) {
-    // TODO: maybe make it 2*distinc(v). Don't know if it's worth it tho.
     nodes.reserve(2*v.size());
     auto [mn, mx] = minmax_element(v.begin(), v.end());
-    build(nodes[make_node(*mn, *mx)], v.begin(), v.end());
+    auto build = [&](auto &&self, Node &node, auto from, auto to) {
+      if (node.lo == node.hi or from >= to) return;
+      auto mid = midpoint(node.lo, node.hi);
+      auto f = [&mid](T x) { return x <= mid; };
+      node.pcnt.reserve(to - from + 1);
+      node.pcnt.push_back(0);
+      node.psum.reserve(to - from + 1);
+      node.psum.push_back(0);
+      T left_upper = node.lo, right_lower = node.hi;
+      for (auto it = from; it != to; it++) {
+        auto value = f(*it);
+        node.pcnt.push_back(node.pcnt.back() + value);
+        node.psum.push_back(node.psum.back() + *it);
+        if (value) left_upper = max(left_upper, *it);
+        else right_lower = min(right_lower, *it);
+      }
+      auto pivot = stable_partition(from, to, f);
+      node.left_child = make_node(node.lo, left_upper);
+      self(self, nodes[node.left_child], from, pivot);
+      node.right_child = make_node(right_lower, node.hi);
+      self(self, nodes[node.right_child], pivot, to);
+    };
+    build(build, nodes[make_node(*mn, *mx)], v.begin(), v.end());
   }
 
-  T kth_element(int l, int r, int k) const {
-    return kth_element(nodes[0], l, r, k);
-  }
-  int count_less(int l, int r, T k) const {
-    constexpr auto base = [](const Node & node, T x) -> int {
-      if (node.lo >= x) return 0;
-      if (node.hi < x) return 1;
-      return -1;
+  T kth_element(int L, int R, int K) const {
+    auto f = [&](auto &&self, const Node &node, int l, int r, int k) -> T {
+      if (l > r) return 0;
+      if (node.lo == node.hi) return node.lo;
+      int lb = node.pcnt[l], rb = node.pcnt[r+1], left_size = rb - lb;
+      return (left_size > k ?
+              self(self, nodes[node.left_child], lb, rb-1, k) :
+              self(self, nodes[node.right_child], l-lb, r-rb, k-left_size));
     };
-    return count_query<T, base>(nodes[0], l, r, k);
+    return f(f, nodes[0], L, R, K);
   }
-  int count_greater(int l, int r, T k) const {
-    constexpr auto base = [](const Node & node, T x) -> int {
-      if (node.hi <= x) return 0;
-      if (node.lo > x) return 1;
-      return -1;
+
+  int count_in_range(int L, int R, T a, T b) const {
+    auto f = [&](auto &&self, const Node &node, int l, int r) -> int {
+      if (l > r or node.lo > b or node.hi < a) return 0;
+      if (a <= node.lo and node.hi <= b) return r - l + 1;
+      int lb = node.pcnt[l], rb = node.pcnt[r+1];
+      return self(self, nodes[node.left_child], lb, rb-1) +
+             self(self, nodes[node.right_child], l-lb, r-rb);
     };
-    return count_query<T, base>(nodes[0], l, r, k);
+    return f(f, nodes[0], L, R);
   }
-  int count_equal(int l, int r, T k) const {
-    constexpr auto base = [](const Node & node, T x) -> int {
-      if (node.lo > x or node.hi < x) return 0;
-      if (node.lo == node.hi) return (x == node.lo);
-      return -1;
+
+  ll sum_in_range(int L, int R, T a, T b) const {
+    auto f = [&](auto &&self, const Node &node, int l, int r) -> ll {
+      if (l > r or node.lo > b or node.hi < a) return 0ll;
+      if (a <= node.lo and node.hi <= b)
+        return (node.lo == node.hi ? (r - l + 1ll) * node.lo : node.psum[r+1] - node.psum[l]);
+      int lb = node.pcnt[l], rb = node.pcnt[r+1];
+      return self(self, nodes[node.left_child], lb, rb-1) +
+             self(self, nodes[node.right_child], l-lb, r-rb);
     };
-    return count_query<T, base>(nodes[0], l, r, k);
-  }
-  int count_in_range(int l, int r, T a, T b) const {
-    constexpr auto base = [](const Node &node, const pair<T, T> &x) -> int {
-      if (node.lo > x.second or node.hi < x.first) return 0;
-      if (x.first <= node.lo and node.hi <= x.second) return 1;
-      return -1;
-    };
-    return count_query<pair<T, T>, base>(nodes[0], l, r, pair<T, T>(a, b));
+    return f(f, nodes[0], L, R);
   }
 private:
-  void build(Node &node, typename vector<T>::iterator from, typename vector<T>::iterator to) {
-    if (node.lo == node.hi or from >= to) return;
-    auto mid = midpoint(node.lo, node.hi);
-    auto f = [&mid](T x) { return x <= mid; };
-    node.b.reserve(to - from + 1);
-    node.b.push_back(0);
-    T left_upper = node.lo, right_lower = node.hi;
-    for (auto it = from; it != to; it++) {
-      auto value = f(*it);
-      node.b.push_back(node.b.back() + value);
-      if (value) left_upper = max(left_upper, *it);
-      else right_lower = min(right_lower, *it);
-    }
-
-    auto pivot = stable_partition(from, to, f);
-    node.left_child = make_node(node.lo, left_upper);
-    build(nodes[node.left_child], from, pivot);
-    node.right_child = make_node(right_lower, node.hi);
-    build(nodes[node.right_child], pivot, to);
-  }
-
-  template <typename K, auto handle_base>
-  int count_query(const Node &node, int l, int r, K k) const {
-    static_assert(is_invocable_r_v<int, decltype(handle_base), Node, K>);
-
-    if (l > r) return 0;
-    {
-      auto b = handle_base(node, k);
-      if (~b) return b * (r - l + 1);
-    }
-    int lb = node.b[l], rb = node.b[r+1];
-    return count_query<K, handle_base>(nodes[node.left_child], lb, rb-1, k) + count_query<K, handle_base>(nodes[node.right_child], l-lb, r-rb, k);
-  }
-
-  T kth_element(const Node &node, int l, int r, int k) const {
-    if (l > r) return 0;
-    if (node.lo == node.hi) return node.lo;
-    int lb = node.b[l], rb = node.b[r+1], left_size = rb - lb;
-    return (left_size > k ? kth_element(nodes[node.left_child], lb, rb-1, k) : kth_element(nodes[node.right_child], l-lb, r-rb, k-left_size));
-  }
-
   int make_node(T lo, T hi) {
     int id = (int)nodes.size();
     nodes.emplace_back(lo, hi);
