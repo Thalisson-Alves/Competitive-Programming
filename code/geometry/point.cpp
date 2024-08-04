5954e8a92c626fdb2dd4c57fa934a31c19ff9c61
Thalisson-Alves <thalisson2030@gmail.com>
2024-08-04 08:27:19 -0300

    split point

---
+++ code/geometry/point.cpp
@@ -0,0 +1,46 @@
+template<typename T=double> struct Point {
+  T x, y;
+
+  Point() : x(0), y(0) {}
+  Point(T x_, T y_) : x(x_), y(y_) {}
+
+  inline Point operator+(const Point &p) const { return Point(x + p.x, y + p.y); }
+  inline Point operator-(const Point &p) const { return Point(x - p.x, y - p.y); }
+  inline Point operator+(const T &k) const { return Point(x + k, y + k); }
+  inline Point operator-(const T &k) const { return Point(x - k, y - k); }
+  inline Point operator*(const T &k) const { return Point(x * k, y * k); }
+  inline Point operator/(const T &k) const { return Point(x / k, y / k); }
+
+  inline Point& operator+=(const Point &p) { x += p.x, y += p.y; return *this; }
+  inline Point& operator-=(const Point &p) { x -= p.x, y -= p.y; return *this; }
+  inline Point& operator+=(const T &k) { x += k, y += k; return *this; }
+  inline Point& operator-=(const T &k) { x -= k, y -= k; return *this; }
+  inline Point& operator*=(const T &k) { x *= k, y *= k; return *this; }
+  inline Point& operator/=(const T &k) { x /= k, y /= k; return *this; }
+
+  inline bool operator==(const Point &p) const { return eq(x, p.x) and eq(y, p.y); }
+  inline bool operator<(const Point &p) const { return eq(x, p.x) ? y < p.y : x < p.x; }
+  inline bool operator>(const Point &p) const { return eq(x, p.x) ? y > p.y : x > p.x; }
+  inline bool operator<=(const Point &p) const { return *this == p or *this < p; }
+  inline bool operator>=(const Point &p) const { return *this == p or *this > p; }
+
+  friend ostream& operator<<(ostream &os, const Point &p) { return os << p.x << ' ' << p.y; }
+  friend istream& operator>>(istream &is, Point &p) { return is >> p.x >> p.y; }
+
+  template<typename U> void rotate(U rad) {
+    tie(x, y) = make_pair(x * cos(rad) - y * sin(rad), x * sin(rad) + y * cos(rad));
+  }
+  template<typename U> Point<U> rotated(U rad) const {
+    return Point<U>(x * cos(rad) - y * sin(rad), x * sin(rad) + y * cos(rad));
+  }
+  inline T dot(const Point &p) const { return x * p.x + y * p.y; }
+  inline T cross(const Point &p) const { return x * p.y - y * p.x; }
+  inline T cross(const Point &a, const Point &b) const { return (a - *this).cross(b - *this); }
+  inline T dist2() const { return x * x + y * y; }
+  inline double dist() const { return hypot(x, y); }
+  inline double angle() const { return atan2(y, x); }
+  inline double norm() const { return sqrt(dot(*this)); }
+  inline Point rot90() const { return Point(-y, x); }
+  inline Point to(const Point &p) const { return p - *this; }
+};
+
