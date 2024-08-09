23ccc9d50d0c0361447d23f8f65a639b98d93ace
Thalisson-Alves <thalisson2030@gmail.com>
2024-08-09 08:23:06 -0300

    0-indexed

--- code/data_structures/bitree.cpp
+++ code/data_structures/bitree.cpp
@@ -1,28 +1,25 @@
-template<typename T>
-struct BITree
-{
-    int N;
-    vector<T> v;
+template<typename T> struct BITree {
+  int N;
+  vector<T> v;
 
-    BITree(int n) : N(n), v(n+1, 0) {}
+  BITree(int n) : N(n), v(n+2, 0) {}
 
-    void update(int i, const T& x)
-    {
-        if (i == 0) return;
-        for (; i <= N; i += i & -i)
-            v[i] += x;
-    }
+  void update(int i, T x) {
+    for (++i; i <= N; i += i & -i)
+      v[i] += x;
+  }
 
-    T range_sum(int i, int j)
-    {
-        return range_sum(j) - range_sum(i - 1);
-    }
+  void update(int l, int r, T x) {
+    update(l, x);
+    update(r+1, -x);
+  }
 
-    T range_sum(int i)
-    {
-        T sum = 0;
-        for (; i > 0; i -= i & -i)
-            sum += v[i];
-        return sum;
-    }
+  T query(int i, int j) { return query(j) - query(i - 1); }
+
+  T query(int i) {
+    T sum = 0;
+    for (++i; i > 0; i -= i & -i)
+      sum += v[i];
+    return sum;
+  }
 };
