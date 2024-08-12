70145e0c9b483e2a965b67673bb154639950aa0f
Thalisson-Alves <thalisson2030@gmail.com>
2024-08-12 19:15:07 -0300

    add binary search

--- code/data_structures/segtree.cpp
+++ code/data_structures/segtree.cpp
@@ -1,49 +1,82 @@
-template <typename T, typename F> struct SegTree {
-  int N;
-  const T identity = T();
+#define F(expr) [](auto a, auto b) { return expr; }
+template <typename T, typename Op = T(*)(T,T)> struct SegTree {
+  int size;
   vector<T> ns;
-  const F op;
+  const Op op;
+  const T id = T();
 
-  SegTree(int n, F f = F(), const T identity_ = T()) : identity(identity_), op(f) {
-    N = 1;
-    while (N < n) N <<= 1;
-    ns.resize(N << 1, identity);
+  SegTree(int n, Op f = Op(), const T identity = T()) : op(f), id(identity) {
+    size = 1;
+    while (size < n) size <<= 1;
+    ns.resize(size << 1, id);
   }
-  SegTree(const vector<T> &v, F f = F()) : SegTree((int)v.size(), f) {
-    copy(v.begin(), v.end(), ns.begin() + N);
-    for (int i = N - 1; i > 0; --i)
+  SegTree(const vector<T> &v, Op f = Op()) : SegTree((int)v.size(), f) {
+    copy(v.begin(), v.end(), ns.begin() + size);
+    for (int i = size - 1; i > 0; --i)
       ns[i] = op(ns[2 * i], ns[2 * i + 1]);
   }
 
-  T query(int i) const {
-    return ns[i + N];
-  }
-
+  T query(int i) const { return ns[i + size]; }
   T query(int l, int r) const {
-    auto ml = identity, mr = identity;
-    for (l += N, r += N; l <= r; l >>= 1, r >>= 1) {
+    auto ml = id, mr = id;
+    for (l += size, r += size; l <= r; l >>= 1, r >>= 1) {
       if (l & 1) ml = op(ml, ns[l++]);
       if (not (r & 1)) mr = op(ns[r--], mr);
     }
     return op(ml, mr);
   }
 
-  int kth(int k) const {
-    int i;
-    for (i = 1; 2*i < (int)ns.size();) {
-      if (ns[2*i] > k) i <<= 1;
-      else k -= ns[2*i], i = i*2+1;
-    }
-    return i-N;
-  }
-
-  void update(int i, T value) {
-    set(i, op(ns[i + N], value));
-  }
-
+  void update(int i, T value) { set(i, op(ns[i + size], value)); }
   void set(int i, T value) {
-    ns[i += N] = value;
+    ns[i += size] = value;
     while (i >>= 1)
       ns[i] = op(ns[2 * i], ns[2 * i + 1]);
+  }
+
+  template <typename F> int pos(F f) {
+    int u = 1;
+    if (!f(ns[u])) return -1;
+    for (auto sm = id; u < size;) {
+      u <<= 1;
+      if (!f(op(sm, ns[u])))
+        sm = op(sm, ns[u++]);
+    }
+    return u-size;
+  }
+
+  template <typename F> int find_right(int l, F f) {
+    auto sm = id;
+    l += size;
+    do {
+      while (!(l&1)) l >>= 1;
+      if (f(op(sm, ns[l]))) {
+        while (l < size) {
+          l <<= 1;
+          if (!f(op(sm, ns[l])))
+            sm = op(sm, ns[l++]);
+        }
+        return l-size;
+      }
+      sm = op(sm, ns[l++]);
+    } while ((l&-l) != l);
+    return -1;
+  }
+
+  template <typename F> int find_left(int r, F f) {
+    auto sm = id;
+    r += size;
+    do {
+      while (r > 1 and (r&1)) r >>= 1;
+      if (f(op(ns[r], sm))) {
+        while (r < size) {
+          r = r<<1|1;
+          if (!f(op(ns[r], sm)))
+            sm = op(ns[r--], sm);
+        }
+        return r-size;
+      }
+      sm = op(ns[r--], sm);
+    } while ((r&-r) != r);
+    return -1;
   }
 };
