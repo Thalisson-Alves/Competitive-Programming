4db74e1164d77a4cc838a173f4851ad646908ca6
Thalisson-Alves <thalisson2030@gmail.com>
2024-08-14 17:16:31 -0300

    use heap allocation

--- code/data_structures/sparse_segtree.cpp
+++ code/data_structures/sparse_segtree.cpp
@@ -1,51 +1,58 @@
 #define F(exp) [](auto a, auto b) { return exp; }
-template <typename T, auto op, size_t Size=0x989680>
-struct SparseSegTree {
-  static_assert(std::is_convertible_v<decltype(op), std::function<T(T, T)>>,
-                "Operation must be convertible to std::function<T(T, T)>");
-
+template <typename T, typename Op = T(*)(T,T)> struct SparseSegTree {
   struct Node {
     T value;
-    int left_child, right_child;
-
-    Node() : value(T()), left_child(0), right_child(0) {}
+    unique_ptr<Node> l, r;
   };
 
-  static int cur_pos;
-  static Node nodes[Size];
-  const T identity;
+  unique_ptr<Node> root;
+  const Op op;
+  const T id;
   const pair<int, int> range;
 
-  SparseSegTree(int l, int r, T id = T()) : identity(id), range(l, r) { }
+  SparseSegTree(int l, int r, Op f = Op(), T id_ = T()) : root(make_unique<Node>(id_)), op(f), id(id_), range(l, r) { }
 
   T query(int idx) { return query(idx, idx); }
-  T query(int l, int r) { return query(nodes[0], l, r, range.first, range.second); }
+  T query(int l, int r) { return query(root, l, r, range.first, range.second); }
+  int pos(const auto &f) {
+    if (!f(root->value)) return -1;
+    return pos(root, range.first, range.second, f, id);
+  }
 
 private:
-  T query(Node &node, int l, int r, int tl, int tr) {
+  T query(unique_ptr<Node> &node, int l, int r, int tl, int tr) {
     if (r < tl or tr < l)
-      return identity;
+      return id;
 
-    if (l <= tl and tr <= r) {
-      return node.value;
-    }
+    if (l <= tl and tr <= r)
+      return node->value;
 
     extend(node);
     auto tm = tl + (tr - tl) / 2;
-    return op(query(nodes[node.left_child], l, r, tl, tm), query(nodes[node.right_child], l, r, tm+1, tr));
+    return op(query(node->l, l, r, tl, tm), query(node->r, l, r, tm+1, tr));
+  }
+  int pos(unique_ptr<Node> &node, int tl, int tr, const auto &f, T acc) {
+    if (tl == tr)
+      return tl;
+
+    extend(node);
+    auto tm = tl + (tr - tl) / 2;
+    if (f(op(acc, node->l->value)))
+      return pos(node->l, tl, tm, f, acc);
+    return pos(node->r, tm+1, tr, f, op(acc, node->l->value));
   }
 
 public:
-  void set(int idx, const T x) { update<1>(nodes[0], idx, x, range.first, range.second); }
-  void update(int idx, const T x) { update<0>(nodes[0], idx, x, range.first, range.second); }
+  void set(int idx, const T x) { update<1>(root, idx, x, range.first, range.second); }
+  void update(int idx, const T x) { update<0>(root, idx, x, range.first, range.second); }
 
 private:
-  template<bool is_setting> void update(Node &node, int idx, const T x, int tl, int tr) {
+  template<bool is_setting> void update(unique_ptr<Node> &node, int idx, const T x, int tl, int tr) {
     if (tl == tr) {
       if constexpr (is_setting) {
-        node.value = x;
+        node->value = x;
       } else {
-        node.value = op(node.value, x);
+        node->value = op(node->value, x);
       }
       return;
     }
@@ -53,17 +60,14 @@ private:
     extend(node);
     auto tm = tl + (tr - tl) / 2;
     (idx <= tm
-     ? update<is_setting>(nodes[node.left_child], idx, x, tl, tm)
-     : update<is_setting>(nodes[node.right_child], idx, x, tm+1, tr));
-    node.value = op(nodes[node.left_child].value, nodes[node.right_child].value);
+     ? update<is_setting>(node->l, idx, x, tl, tm)
+     : update<is_setting>(node->r, idx, x, tm+1, tr));
+    node->value = op(node->l->value, node->r->value);
   }
 
-  void extend(Node &node) {
-    if (node.left_child) return;
-    assert(cur_pos < (int)Size);
-    nodes[node.left_child = cur_pos++].value = identity;
-    nodes[node.right_child = cur_pos++].value = identity;
+  void extend(unique_ptr<Node> &node) {
+    if (node->l) return;
+    (node->l = make_unique<Node>())->value = id;
+    (node->r = make_unique<Node>())->value = id;
   }
 };
-template <typename T, auto op, size_t Size> SparseSegTree<T, op, Size>::Node SparseSegTree<T, op, Size>::nodes[Size];
-template <typename T, auto op, size_t Size> int SparseSegTree<T, op, Size>::cur_pos = 1;
