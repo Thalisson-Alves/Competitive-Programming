ea35af1310508089938787d28af56f99cd3012b0
Thalisson-Alves <thalisson2030@gmail.com>
2024-08-27 20:49:25 -0300

    make hashstring more generic

--- code/strings/hash_string.cpp
+++ code/strings/hash_string.cpp
@@ -1,63 +1,53 @@
-template <int p1=31, int p2=29, int q1=1000000007, int q2=1000000009>
-struct HashString {
-  vector<pair<int, int>> suf, powers;
-
-  HashString(const string &s) : suf(s.size()+1), powers(s.size()+1) {
-    powers[0] = {1, 1};
-    powers[1] = {p1, p2};
-
-    for (int i = (int)s.size()-1; ~i; --i) {
-      suf[i].first = (int)((s[i] + (ll)suf[i+1].first * powers[1].first) % q1);
-      suf[i].second = (int)((s[i] + (ll)suf[i+1].second * powers[1].second) % q2);
-    }
-    for (int i = 2; i <= (int)s.size(); i++) {
-      powers[i].first = (int)((ll)powers[i-1].first * powers[1].first % q1);
-      powers[i].second = (int)((ll)powers[i-1].second * powers[1].second % q2);
-    }
+template<int p1=31, int p2=29, int q1=(int)1e9+7, int q2=(int)1e9+9> struct HashString {
+  using H = pair<int, int>;
+  static H mul(H a, H b) { return {(ll)a.first*b.first%q1, (ll)a.second*b.second%q2}; }
+  static H add(H a, int x) { return {(a.first+x)%q1, (a.second+x)%q2}; }
+  static H sub(H a, H b) { return {(a.first-b.first+q1)%q1, (a.second-b.second+q2)%q2}; }
+  static inline vector<H> pows{{1, 1}, {p1, p2}};
+  static inline H power(int i) {
+    while ((int)pows.size() <= i) pows.push_back(mul(pows.back(), pows[1]));
+    return pows[i];
   }
-
-  bool operator<(const HashString &a) const {
-    return hash() < a.hash();
+  vector<H> pref;
+  HashString() : pref(1) {}
+  HashString(const auto &s) : HashString() { add(s); }
+  void add(int c) { pref.push_back(add(mul(pref.back(), pows[1]), c)); }
+  void add(const string &s) {
+    reserve_more(s.size());
+    for (auto c : s) add(c);
   }
-
-  pair<int, int> substr(int l) const {
-    return substr(l, size()-1);
+  template <typename T> void add(const vector<T> &s) {
+    reserve_more(s.size());
+    for (auto c : s) add(c);
   }
-
-  pair<int, int> substr(int l, int r) const {
-    assert(0 <= l and l <= r and r < (int)suf.size() - 1);
-    int a = (int)((((suf[l].first - (ll)suf[r+1].first * powers[r-l+1].first) % q1) + q1) % q1);
-    int b = (int)((((suf[l].second - (ll)suf[r+1].second * powers[r-l+1].second) % q2) + q2) % q2);
-    return {a, b};
+  void reserve_more(size_t n) {
+    pref.reserve(pref.size() + n);
+    pows.reserve(pref.capacity());
   }
-
+  H hash() const { return pref.back(); }
+  int size() const { return (int)pref.size() - 1; }
+  bool operator<(const HashString &a) const { return hash() < a.hash(); }
+  H substr(int l) const { return substr(l, size()-1); }
+  H substr(int l, int r) {
+    assert(0 <= l and l <= r and r < size());
+    return sub(pref[r+1], mul(power(r-l+1), pref[l]));
+  }
   int max_prefix(const HashString &h) const {
     int l = 0, r = (int)min(size(),h.size())-1;
     while (l <= r) {
       int mid = l + (r - l) / 2;
-      if (substr(0, mid) == h.substr(0, mid)) {
-        l = mid + 1;
-      } else {
-        r = mid - 1;
-      }
+      if (substr(0, mid) == h.substr(0, mid)) l = mid + 1;
+      else r = mid - 1;
     }
     return l;
   }
-
   int max_suffix(const HashString &h) const {
     int l = 1, r = (int)min(size(),h.size());
     while (l <= r) {
       int mid = l + (r - l) / 2;
-      if (substr(size()-mid) == h.substr(h.size()-mid)) {
-        l = mid + 1;
-      } else {
-        r = mid - 1;
-      }
+      if (substr(size()-mid) == h.substr(h.size()-mid)) l = mid + 1;
+      else r = mid - 1;
     }
     return r;
   }
-
-  pair<int, int> hash() const { return suf[0]; }
-
-  int size() const { return (int)suf.size() - 1; }
 };
