79c6dbb070058642ca8192458bdba86b41686b5d
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-02 19:27:50 -0300

    format

--- code/primitives/matrix.cpp
+++ code/primitives/matrix.cpp
@@ -1,16 +1,12 @@
 template <typename T> struct Matrix {
   vector<vector<T>> d;
-
   Matrix() : Matrix(0) {}
   Matrix(int n) : Matrix(n, n) {}
   Matrix(int n, int m) : Matrix(vector<vector<T>>(n, vector<T>(m))) {}
   Matrix(const vector<vector<T>> &v) : d(v) {}
-
   constexpr int n() const { return (int)d.size(); }
   constexpr int m() const { return n() ? (int)d[0].size() : 0; }
-
   void rotate() { *this = rotated(); }
-
   Matrix<T> rotated() const {
     Matrix<T> res(m(), n());
     for (int i = 0; i < m(); i++) {
@@ -20,32 +16,28 @@ template <typename T> struct Matrix {
     }
     return res;
   }
-
-  Matrix<T> pow(ll power) const {
+  Matrix<T> pow(ll p) { return Matrix<T>(*this).ipow(p); }
+  Matrix<T> &ipow(ll p) {
     assert(n() == m());
-
     auto res = Matrix<T>::identity(n());
-    auto b = *this;
-    while (power) {
-      if (power & 1) res *= b;
-      b *= b;
-      power >>= 1;
+    while (p) {
+      if (p & 1) res *= *this;
+      *this *= *this;
+      p >>= 1;
     }
-    return res;
+    swap(res.d, d);
+    return *this;
   }
-
   Matrix<T> submatrix(int start_i, int start_j, int rows = INT_MAX, int cols = INT_MAX) const {
     rows = min(rows, n() - start_i);
     cols = min(cols, m() - start_j);
     if (rows <= 0 or cols <= 0) return {};
-
     Matrix<T> res(rows, cols);
     for (int i = 0; i < rows; i++)
       for (int j = 0; j < cols; j++)
         res[i][j] = d[i+start_i][j+start_j];
     return res;
   }
-
   Matrix<T> translated(int x, int y) const {
     Matrix<T> res(n(), m());
     for (int i = 0; i < n(); i++) {
@@ -56,16 +48,14 @@ template <typename T> struct Matrix {
     }
     return res;
   }
-
   static Matrix<T> identity(int n) {
     Matrix<T> res(n);
-    for (int i = 0; i < n; i++)
-      res[i][i] = 1;
+    for (int i = 0; i < n; i++) res[i][i] = 1;
     return res;
   }
-
   vector<T> &operator[](int i) { return d[i]; }
   const vector<T> &operator[](int i) const { return d[i]; }
+  Matrix<T> operator+(T value) const { return Matrix(*this) += value; }
   Matrix<T> &operator+=(T value) {
     for (auto &row : d) {
       for (auto &x : row)
@@ -73,14 +63,7 @@ template <typename T> struct Matrix {
     }
     return *this;
   }
-  Matrix<T> operator+(T value) const {
-    auto res = *this;
-    for (auto &row : res) {
-      for (auto &x : row)
-        x = x + value;
-    }
-    return res;
-  }
+  Matrix<T> operator-(T value) const { return Matrix(*this) -= value; }
   Matrix<T> &operator-=(T value) {
     for (auto &row : d) {
       for (auto &x : row)
@@ -88,28 +71,13 @@ template <typename T> struct Matrix {
     }
     return *this;
   }
-  Matrix<T> operator-(T value) const {
-    auto res = *this;
-    for (auto &row : res) {
-      for (auto &x : row)
-        x = x - value;
-    }
-    return res;
-  }
+  Matrix<T> operator*(T value) const { return Matrix(*this) *= value; }
   Matrix<T> &operator*=(T value) {
     for (auto &row : d) {
       for (auto &x : row)
         x *= value;
     }
     return *this;
-  }
-  Matrix<T> operator*(T value) const {
-    auto res = *this;
-    for (auto &row : res) {
-      for (auto &x : row)
-        x = x * value;
-    }
-    return res;
   }
   Matrix<T> &operator/=(T value) {
     for (auto &row : d) {
@@ -118,14 +86,7 @@ template <typename T> struct Matrix {
     }
     return *this;
   }
-  Matrix<T> operator/(T value) const {
-    auto res = *this;
-    for (auto &row : res) {
-      for (auto &x : row)
-        x = x / value;
-    }
-    return res;
-  }
+  Matrix<T> operator/(T value) const { return Matrix(*this) /= value; }
   Matrix<T> &operator+=(const Matrix<T> &o) {
     assert(n() == o.n() and m() == o.m());
     for (int i = 0; i < n(); i++) {
@@ -135,16 +96,7 @@ template <typename T> struct Matrix {
     }
     return *this;
   }
-  Matrix<T> operator+(const Matrix<T> &o) const {
-    assert(n() == o.n() and m() == o.m());
-    auto res = *this;
-    for (int i = 0; i < n(); i++) {
-      for (int j = 0; j < m(); j++) {
-        res[i][j] = res[i][j] + o[i][j];
-      }
-    }
-    return res;
-  }
+  Matrix<T> operator+(const Matrix<T> &o) const { return Matrix(o) += o; }
   Matrix<T> &operator-=(const Matrix<T> &o) {
     assert(n() == o.n() and m() == o.m());
     for (int i = 0; i < n(); i++) {
@@ -154,34 +106,21 @@ template <typename T> struct Matrix {
     }
     return *this;
   }
-  Matrix<T> operator-(const Matrix<T> &o) const {
-    assert(n() == o.n() and m() == o.m());
-    auto res = *this;
-    for (int i = 0; i < n(); i++) {
-      for (int j = 0; j < m(); j++) {
-        res[i][j] = res[i][j] - o[i][j];
-      }
-    }
-    return res;
-  }
+  Matrix<T> operator-(const Matrix<T> &o) const { return Matrix(*this) -= o; }
+  Matrix<T> operator*(const Matrix<T> &o) const { return Matrix(*this) *= o; }
   Matrix<T> &operator*=(const Matrix<T> &o) {
-    *this = *this * o;
-    return *this;
-  }
-  Matrix<T> operator*(const Matrix<T> &o) const {
     assert(m() == o.n());
     Matrix<T> res(n(), o.m());
     for (int i = 0; i < res.n(); i++) {
       for (int j = 0; j < res.m(); j++) {
-        auto &x = res[i][j];
         for (int k = 0; k < m(); k++) {
-          x += (d[i][k] * o[k][j]);
+          res[i][k] += (d[i][j] * o[j][k]);
         }
       }
     }
-    return res;
+    swap(d, res.d);
+    return *this;
   }
-
   friend istream &operator>>(istream &is, Matrix<T> &mat) {
     for (auto &row : mat)
       for (auto &x : row)
@@ -194,22 +133,18 @@ template <typename T> struct Matrix {
       if (not frow) os << '\n';
       bool first = 1;
       for (auto &x : row) {
-        if (not first)
-          os << ' ';
+        if (not first) os << ' ';
         os << x;
         first = 0;
       }
-
       frow = 0;
     }
     return os;
   }
-
   auto begin() { return d.begin(); }
   auto end() { return d.end(); }
   auto rbegin() { return d.rbegin(); }
   auto rend() { return d.rend(); }
-
   auto begin() const { return d.begin(); }
   auto end() const { return d.end(); }
   auto rbegin() const { return d.rbegin(); }
--- code/primitives/modint.cpp
+++ code/primitives/modint.cpp
@@ -1,25 +1,19 @@
 template <uint_fast64_t Modulus> class modint {
   using u64 = uint_fast64_t;
-
 public:
   using value_type = u64;
-
   static constexpr u64 mod = Modulus;
-
 private:
   u64 v;
-
   constexpr modint &negate() noexcept {
     if (v != 0) v = mod - v;
     return *this;
   }
-
 public:
   constexpr modint() noexcept : v(0) {}
   template <typename T> constexpr modint(const T x) noexcept : v(safe_mod(x)) {}
   constexpr u64 &value() noexcept { return v; }
   constexpr const u64 &value() const noexcept { return v; }
-
   template <typename T> static constexpr u64 safe_mod(T x) noexcept {
     if constexpr (is_signed_v<T>) {
       return (x % (ll)mod + mod) % mod;
@@ -27,21 +21,17 @@ public:
       return x % mod;
     }
   }
-
   constexpr bool operator==(const modint rhs) const noexcept { return v == rhs.v; }
   constexpr bool operator!=(const modint rhs) const noexcept { return v != rhs.v; }
   constexpr bool operator<(const modint rhs) const noexcept { return v < rhs.v; }
   constexpr bool operator<=(const modint rhs) const noexcept { return v <= rhs.v; }
   constexpr bool operator>(const modint rhs) const noexcept { return v > rhs.v; }
   constexpr bool operator>=(const modint rhs) const noexcept { return v >= rhs.v; }
-
   template <typename T> constexpr explicit operator T() const noexcept { return (T)v; }
-
   constexpr modint operator++(int) noexcept { modint res = *this; *this += 1; return res; }
   constexpr modint operator--(int) noexcept { modint res = *this; *this -= 1; return res; }
   constexpr modint &operator++() noexcept { return *this += 1; }
   constexpr modint &operator--() noexcept { return *this -= 1; }
-
   constexpr modint operator+() const noexcept { return modint(*this); }
   constexpr modint operator-() const noexcept { return modint(*this).negate(); }
   friend constexpr modint operator+(const modint lhs, const modint rhs) noexcept { return modint(lhs) += rhs; }
@@ -62,20 +52,9 @@ public:
     v = v * rhs.v % mod;
     return *this;
   }
-  constexpr modint &operator/=(modint rhs) noexcept {
-    static_assert(mod < static_cast<u64>(1) << 32, "Modulus must be less than 2**32");
-
-    u64 exp = mod - 2;
-    while (exp) {
-      if (exp & 1) *this *= rhs;
-      rhs *= rhs;
-      exp /= 2;
-    }
-    return *this;
-  }
+  constexpr modint &operator/=(modint rhs) noexcept { return *this * rhs.inv(); }
   constexpr modint pow(u64 x) const {
     static_assert(mod < static_cast<u64>(1) << 32, "Modulus must be less than 2**32");
-
     modint res = 1, a = *this;
     while (x) {
       if (x & 1) res *= a;
@@ -85,18 +64,17 @@ public:
     return res;
   }
   constexpr inline modint inv() const { return pow(mod - 2); }
-
-  friend ostream& operator<<(ostream& s, const modint<mod>& x) {
+  friend ostream& operator<<(ostream& s, const modint &x) {
     s << x.value();
     return s;
   }
-
-  friend istream& operator>>(istream& s, modint<mod>& x) {
-    s >> x.v;
+  friend istream& operator>>(istream& s, modint& x) {
+    ll value;
+    s >> value;
+    x = {value};
     return s;
   }
 };
-
 template <uint_fast64_t Modulus>
 constexpr typename modint<Modulus>::u64 modint<Modulus>::mod;
 using mint = modint<1000000007>;
