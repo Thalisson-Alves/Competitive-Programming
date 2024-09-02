512f14e2c48937e2d72c5b2cf5f55f80902ea2dc
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-01 21:59:39 -0300

    formatting

--- code/geometry/convex_hull.cpp
+++ code/geometry/convex_hull.cpp
@@ -1,38 +1,31 @@
-template<const bool include_collinear=false, typename T>
-vector<Point<T>> convex_hull(vector<Point<T>> pts)
-{
+/* Convex hull
+ * - If `ic` is true, collinear points are included
+ *
+ * Time complexity: O(n log n)
+*/
+template<const bool ic=false, typename T> Polygon<T> convex_hull(Polygon<T> pts) {
   if (pts.size() == 1) return pts;
-
   sort(pts.begin(), pts.end());
   auto p1 = pts[0], p2 = pts.back();
   vector<Point<T>> up, down;
   up.push_back(p1);
   down.push_back(p1);
-  for (size_t i = 1; i < pts.size(); i++)
-  {
-    if (i == pts.size() - 1 or cw<include_collinear>(p1, pts[i], p2))
-    {
-      while (up.size() >= 2 and !cw<include_collinear>(up[up.size()-2], up[up.size()-1], pts[i]))
-        up.pop_back();
+  for (size_t i = 1; i < pts.size(); i++) {
+    if (i == pts.size() - 1 or cw<ic>(p1, pts[i], p2)) {
+      while (up.size() >= 2 and !cw<ic>(up[up.size()-2], up[up.size()-1], pts[i])) up.pop_back();
       up.push_back(pts[i]);
     }
-    if (i == pts.size() - 1 or ccw<include_collinear>(p1, pts[i], p2))
-    {
-      while (down.size() >= 2 and !ccw<include_collinear>(down[down.size()-2], down[down.size()-1], pts[i]))
-        down.pop_back();
+    if (i == pts.size() - 1 or ccw<ic>(p1, pts[i], p2)) {
+      while (down.size() >= 2 and !ccw<ic>(down[down.size()-2], down[down.size()-1], pts[i])) down.pop_back();
       down.push_back(pts[i]);
     }
   }
-
-  if constexpr (include_collinear)
-  {
-    if (up.size() == pts.size())
-    {
+  if constexpr (ic) {
+    if (up.size() == pts.size()) {
       reverse(pts.begin(), pts.end());
       return pts;
     }
   }
-
   up.insert(up.end(), down.rbegin() + 1, down.rend() - 1);
   return up;
 }
