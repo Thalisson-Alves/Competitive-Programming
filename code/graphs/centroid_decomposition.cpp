dd5c46a69a59296e592a09ca8307b5df88a7e8d5
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-03 17:44:05 -0300

    refactor

--- code/graphs/centroid_decomposition.cpp
+++ code/graphs/centroid_decomposition.cpp
@@ -1,50 +1,37 @@
-int dfs_size(const vector<vector<int>> &g, vector<int> &sz, vector<bool> &rem, int u, int p) {
-  sz[u] = 1;
-  for (auto x : g[u]) if (!rem[x] and x != p) {
-    sz[u] += dfs_size(g, sz, rem, x, u);
-  }
-  return sz[u];
-}
-
-int centroid(const vector<vector<int>> &g, vector<int> &sz, vector<bool> &rem, int u, int p=-1) {
-  const int max_sz = dfs_size(g, sz, rem, u, p) >> 1;
-  for (;;) {
-    bool ok = true;
-    for (auto x : g[u]) if (!rem[x] and x != p) {
-      if (sz[x] > max_sz) {
-        p = u;
-        u = x;
-        ok = false;
-        break;
-      }
-    }
-    if (ok) break;
-  }
-  return u;
-}
-
-void dfs_dist(const vector<vector<int>> &g, vector<vector<pair<int, int>>> &ancestor, vector<bool> &rem, int c, int u, int p, int d=0) {
-  ancestor[u].emplace_back(c, d);
-  for (auto x : g[u]) if (!rem[x] and x != p) {
-    dfs_dist(g, ancestor, rem, c, x, u, d+1);
-  }
-}
-
-void centroid_decomposition(const vector<vector<int>> &g, vector<int> &sz, vector<vector<pair<int, int>>> &ancestor, vector<bool> &rem, int u=0) {
-  const int c = centroid(g, sz, rem, u);
-  rem[c] = true;
-  dfs_dist(g, ancestor, rem, c, c, c);
-  for (auto x : g[c]) if (!rem[x]) {
-    centroid_decomposition(g, sz, ancestor, rem, x);
-  }
-}
-
-// vector of pair of (parent centroid, distance)
-vector<vector<pair<int, int>>> centroid_decomposition(const vector<vector<int>> &g) {
+void centroid_decomposition(const vector<vector<int>> &g, auto &&f) {
   vector<int> sz(g.size());
   vector<bool> rem(g.size());
+  auto dfs_size = [&](auto &&self, int u, int p=-1) -> int {
+    sz[u] = 1;
+    for (auto v : g[u]) if (v != p and !rem[v])
+      sz[u] += self(self, v, u);
+    return sz[u];
+  };
+  auto centroid = [&](auto &&self, int u, int p, int tsize) -> int {
+    for (auto v : g[u])
+      if (v != p and !rem[v] and sz[v] >= tsize)
+        return self(self, v, u, tsize);
+    return u;
+  };
+  auto dfs = [&](auto &&self, int u) -> void {
+    const int c = centroid(centroid, u, -1, dfs_size(dfs_size, u) >> 1);
+    rem[c] = 1;
+    f(c, rem);
+    for (auto v : g[c]) if (!rem[v]) self(self, v);
+  };
+  dfs(dfs, 0);
+}
+// vector of pair of (parent centroid, distance)
+vector<vector<pair<int, int>>>  centroid_decomposition(const vector<vector<int>> &g) {
   vector<vector<pair<int, int>>> ancestor(g.size());
-  centroid_decomposition(g, sz, ancestor, rem);
-  for (auto &x : ancestor) reverse(x.begin(), x.end());
+  auto run = [&](int cent, const auto &rem) {
+    auto dfs_dist = [&](auto &&self, int c, int u, int p, int d) -> void {
+      ancestor[u].emplace_back(c, d);
+      for (auto v : g[u]) if (v != p and !rem[v]) self(self, c, v, u, d+1);
+    };
+    dfs_dist(dfs_dist, cent, cent, -1, 0);
+  };
+  centroid_decomposition(g, run);
+  for (auto &x : ancestor) reverse(begin(x), end(x));
   return ancestor;
 }
