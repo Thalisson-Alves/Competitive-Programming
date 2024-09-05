28062d9dfe54dd108a463849bbeb58ca86f6b747
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-05 19:50:58 -0300

    refactor

--- code/graphs/binary_lifting.cpp
+++ code/graphs/binary_lifting.cpp
@@ -1,70 +1,46 @@
 struct BinaryLifting {
-  vector<int> far, level, parent;
-
-  BinaryLifting(const vector<vector<int>> &g, int root=0) : far(g.size(), -1), level(g.size()), parent(g.size(), -1) {
-    level[root] = 1;
+  vector<int> far, depth, parent;
+  BinaryLifting(const vector<vector<int>> &g, int root=0) : far(size(g), -1), depth(size(g)), parent(size(g), -1) {
     vector<int> q{root};
-    q.reserve(g.size());
+    q.reserve(size(g));
     for (int i = 0; i < (int)q.size(); i++) {
       auto u = q[i];
-      for (auto x : g[u]) if (x != parent[u]) {
-        parent[x] = u;
-        level[x] = level[u] + 1;
-
+      for (auto v : g[u]) if (v != parent[u]) {
+        parent[v] = u;
+        depth[v] = depth[u] + 1;
         int p1 = u;
         int p2 = far[u];
-
-        if (p2 > -1 and far[p2] > -1 and level[p1] - level[p2] == level[p2] - level[far[p2]])
-          far[x] = far[p2];
-        else
-          far[x] = p1;
-
-        q.push_back(x);
+        if (~p2 and ~far[p2] and depth[p1] - depth[p2] == depth[p2] - depth[far[p2]]) far[v] = far[p2];
+        else far[v] = p1;
+        q.push_back(v);
       }
     }
   }
-
-  int kth_parent(int node, int k) const
-  {
-    while (node >= 0 and k > 0)
-    {
-      if (far[node] > -1 and level[node] - k <= level[far[node]])
-      {
-        k -= level[node] - level[far[node]];
-        node = far[node];
-      }
-      else
-      {
-        k--;
-        node = parent[node];
-      }
-    }
-
-    return node;
-  }
-
-  int lca(int u, int v) const {
-    if (level[u] < level[v]) swap(u, v);
-
-    while (level[v] < level[u]) {
-      if (far[u] > -1 and level[v] <= level[far[u]])
+  int kth_parent(int u, int k) const {
+    while (~u and k > 0) {
+      if (~far[u] and depth[u] - k < depth[far[u]]) {
+        k -= depth[u] - depth[far[u]];
         u = far[u];
-      else
-        u = parent[u];
-    }
-
-    if (u == v) return u;
-
-    while (parent[u] != parent[v]) {
-      if (far[u] > -1 and far[v] > -1 and far[u] != far[v]) {
-        u = far[u];
-        v = far[v];
       } else {
+        k--;
         u = parent[u];
-        v = parent[v];
       }
     }
-
+    return u;
+  }
+  int lca(int u, int v) const {
+    if (depth[u] < depth[v]) swap(u, v);
+    while (depth[v] < depth[u]) {
+      if (~far[u] and depth[v] <= depth[far[u]]) u = far[u];
+      else u = parent[u];
+    }
+    if (u == v) return u;
+    while (parent[u] != parent[v]) {
+      if (~far[u] and ~far[v] and far[u] != far[v])
+        u = far[u], v = far[v];
+      else
+        u = parent[u], v = parent[v];
+    }
     return parent[u];
   }
 };
