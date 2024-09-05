c63c43944918fc5558f2b10a0f4ff40806115414
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-04 21:03:58 -0300

    refactor

--- code/data_structures/sparse_table.cpp
+++ code/data_structures/sparse_table.cpp
@@ -1,49 +1,33 @@
 #define F(expr) [](auto a, auto b) { return expr; }
-template <typename T> struct SparseTable {
-  using Operation = T(*)(T, T);
-
-  int LOGN;
+template <typename T, typename Op> struct SparseTable {
   vector<vector<T>> st;
-  Operation f;
-  T identity;
-
-  static constexpr int log2_floor(unsigned long long i) noexcept { return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1; }
-
+  Op f;
+  const T id;
   // Lazy loading constructor. Needs to call build!
-  SparseTable(Operation op, const T neutral = T()) : LOGN(), st(), f(op), identity(neutral) {}
-
-  SparseTable(const vector<T> &v) : SparseTable(v, F(max(a,b))) {}
-
-  SparseTable(const vector<T> &v, Operation op, const T neutral = T()) : LOGN(), st(), f(op), identity(neutral) {
-    build(v);
-  }
-
+  SparseTable(Op op, const T neutral = T()) : st(), f(op), id(neutral) {}
+  SparseTable(const vector<T> &v, Op op = Op(), const T neutral = T()) : st(), f(op), id(neutral) { build(v); }
   void build(const vector<T> &v) {
-    LOGN = log2_floor(v.size());
-    st.resize(LOGN+1, vector<T>(v.size()));
-
+    auto logn = 31-__builtin_clz((int)size(v));
+    st.resize(logn+1, vector<T>(v.size()));
     st[0] = v;
-    for (int i = 1; i <= LOGN; i++)
+    for (int i = 1; i <= logn; i++)
       for (int j = 0; j + (1 << i) <= (int)v.size(); j++)
         st[i][j] = f(st[i-1][j], st[i-1][j+(1 << (i-1))]);
   }
-
+  T query(int l, int r) const {
+    assert(l <= r);
+    auto lg = 31-__builtin_clz(r-l+1);
+    return f(st[lg][l], st[lg][r-(1<<lg)+1]);
+  }
   // Non RMQ query
   T query_complete(int l, int r) const {
     assert(l <= r);
-    T acc = identity;
-    for (int i = LOGN; ~i; --i)
-      if ((1 << i) <= r - l + 1)
-      {
+    T acc = id;
+    for (int i = 31-__builtin_clz(size(st[0])); ~i; --i)
+      if ((1 << i) <= r - l + 1) {
         acc = f(acc, st[i][l]);
         l += 1 << i;
       }
     return acc;
-  }
-
-  T query(int l, int r) const {
-    assert(l <= r);
-    auto lg = log2_floor(r-l+1);
-    return f(st[lg][l], st[lg][r - (1 << lg) + 1]);
   }
 };
--- code/data_structures/ufds.cpp
+++ code/data_structures/ufds.cpp
@@ -1,15 +1,11 @@
 struct UFDS {
   vector<int> ps, sz;
   int components;
-
   UFDS(int n = 0) : ps(n), sz(n, 1), components(n) {
     iota(ps.begin(), ps.end(), 0);
   }
-
   int find(int x) { return (x == ps[x] ? x : (ps[x] = find(ps[x]))); }
-
   bool same(int x, int y) { return find(x) == find(y); }
-
   bool join(int x, int y) {
     x = find(x);
     y = find(y);
@@ -20,7 +16,6 @@ struct UFDS {
     --components;
     return true;
   }
-
   int add() {
     int id = (int)ps.size();
     ps.push_back(id);
