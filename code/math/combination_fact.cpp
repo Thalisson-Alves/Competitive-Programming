a07e57c0a7bc6d43df4365a26bc3d17220e8df36
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-08 13:48:04 -0300

    add combination_fact

---
+++ code/math/combination_fact.cpp
@@ -0,0 +1,12 @@
+template <typename T=mint> T factorial(int n) {
+  static vector<T> fact{1};
+  while ((int)size(fact) <= n) fact.push_back(fact.back() * size(fact));
+  return fact[n];
+}
+template <typename T=mint> T comb(int n, int k) {
+  return factorial<T>(n) / (factorial<T>(n-k) * factorial<T>(k));
+}
+// same as comb(2*n, n) / (n + 1)
+template <typename T=mint> T catalan(int n) {
+  return factorial<T>(2*n) / (factorial<T>(n) * factorial<T>(n + 1));
+}
