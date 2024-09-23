a0bd8de99163d675c7fc3e80edf7a75220209a74
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-23 13:09:23 -0300

    add more functionality

--- code/data_structures/treap.cpp
+++ code/data_structures/treap.cpp
@@ -42,63 +42,100 @@ template <typename T> struct Treap {
     if (l->priority > r -> priority) return l->r = merge(l->r, r), l->update(), l;
     else return r->l = merge(l, r->l), r->update(), r;
   }
-  pair<ptr, ptr> split(ptr t, int pos) {
+  pair<ptr, ptr> split_at(ptr t, int pos) {
     if (!t) return {};
     t->prop();
     pair<ptr, ptr> res;
-    if (size(t->l) >= pos) res = split(t->l, pos), t->l = res.second, res = {res.first, t};
-    else res = split(t->r, pos-size(t->l)-1), t->r = res.first, res = {t, res.second};
+    if (size(t->l) >= pos) res = split_at(t->l, pos), t->l = res.second, res = {res.first, t};
+    else res = split_at(t->r, pos-size(t->l)-1), t->r = res.first, res = {t, res.second};
+    t->update();
+    return res;
+  }
+  pair<ptr, ptr> split_less(ptr t, T key) {
+    if (!t) return {};
+    t->prop();
+    pair<ptr, ptr> res;
+    if (t->key >= key) res = split_less(t->l, key), t->l = res.second, res = {res.first, t};
+    else res = split_less(t->r, key), t->r = res.first, res = {t, res.second};
     t->update();
     return res;
   }
   T query(int l, int r) {
-    auto p1 = split(root, l);
-    auto p2 = split(p1.second, r-l+1);
+    auto p1 = split_at(root, l);
+    auto p2 = split_at(p1.second, r-l+1);
     T res = query(p2.first);
     root = merge(merge(p1.first, p2.first), p2.second);
     return res;
   }
   T get(int pos) {
-    auto p1 = split(root, pos);
-    auto p2 = split(p1.second, 1);
+    assert(pos < size(root));
+    auto p1 = split_at(root, pos);
+    auto p2 = split_at(p1.second, 1);
     T res = p2.first->key;
     root = merge(merge(p1.first, p2.first), p2.second);
     return res;
   }
   void set(int pos, T s) {
-    auto p1 = split(root, pos);
-    auto p2 = split(p1.second, 1);
+    assert(pos < size(root));
+    auto p1 = split_at(root, pos);
+    auto p2 = split_at(p1.second, 1);
+    p2.first->prop();
     p2.first->key = s;
+    p2.first->update();
     root = merge(merge(p1.first, p2.first), p2.second);
   }
   void update(int l, int r, T s) {
-    auto p1 = split(root, l);
-    auto p2 = split(p1.second, r-l+1);
-    p2.first->lazy += s;
+    auto p1 = split_at(root, l);
+    auto p2 = split_at(p1.second, r-l+1);
+    if (p2.first) p2.first->lazy += s;
     root = merge(merge(p1.first, p2.first), p2.second);
   }
   T query_pref(int r) {
-    auto p = split(root, r+1);
+    auto p = split_at(root, r+1);
     T res = query(p.first);
     root = merge(p.first, p.second);
     return res;
   }
-  void insert(int pos, T value) {
-    auto p = split(root, pos);
+  void insert_at(int pos, T value) {
+    auto p = split_at(root, pos);
     ptr node = new Node(value);
     root = merge(merge(p.first, node), p.second);
   }
-  void push_back(T value) { root = merge(root, new Node(value)); }
-  void remove(int pos) {
-    auto p1 = split(root, pos);
-    auto p2 = split(p1.second, 1);
+  void insert(T key) {
+    auto p = split_less(root, key);
+    ptr node = new Node(key);
+    root = merge(merge(p.first, node), p.second);
+  }
+  int count(T key) {
+    auto p1 = split_less(root, key);
+    auto p2 = split_less(p1.second, key+1);
+    int res = size(p2.first);
+    root = merge(merge(p1.first, p2.first), p2.second);
+    return res;
+  }
+  int count_less(T key) {
+    auto p = split_less(root, key);
+    int res = size(p.first);
+    root = merge(p.first, p.second);
+    return res;
+  }
+  void push_back(T key) { root = merge(root, new Node(key)); }
+  void remove_at(int pos) {
+    auto p1 = split_at(root, pos);
+    auto p2 = split_at(p1.second, 1);
+    delete p2.first;
+    root = merge(p1.first, p2.second);
+  }
+  void remove(T key) {
+    auto p1 = split_less(root, key);
+    auto p2 = split_less(p1.second, key + 1);
     delete p2.first;
     root = merge(p1.first, p2.second);
   }
   void reverse(int l, int r) {
-    auto p1 = split(root, l);
-    auto p2 = split(p1.second, r-l+1);
-    p2.first->rev ^= 1;
+    auto p1 = split_at(root, l);
+    auto p2 = split_at(p1.second, r-l+1);
+    if (p2.first) p2.first->rev ^= 1;
     root = merge(merge(p1.first, p2.first), p2.second);
   }
   void inorder(auto &&f) const {
