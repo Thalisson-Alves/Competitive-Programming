a688869eb285d7c7da675ec9bf2ad7803bbbbe3a
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-25 20:26:09 -0300

    formatting

--- debug.cpp
+++ debug.cpp
@@ -1,97 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 /******** Debug Code *******/
-template<typename T>
-void __print(const T &x) { cerr << x; }
-template <typename A, typename B>
-void __print(const pair<A, B> &p);
-template <typename... A>
-void __print(const tuple<A...> &t);
-template <typename T>
-void __print(stack<T> s);
-template <typename T>
-void __print(queue<T> q);
-template <typename T, typename... U>
-void __print(priority_queue<T, U...> q);
-template <typename A>
-void __print(const A &x);
-template <typename T>
-void __print(optional<T> x);
+template <typename T> void __print(const T &x) { cerr << x; }
+template <typename A, typename B> void __print(const pair<A, B> &p);
+template <typename... A> void __print(const tuple<A...> &t);
+template <typename T> void __print(stack<T> s);
+template <typename T> void __print(queue<T> q);
+template <typename T, typename... U> void __print(priority_queue<T, U...> q);
+template <typename A> void __print(const A &x);
+template <typename T> void __print(optional<T> x);
 void __print(char x);
 void __print(bool x) { cerr << boolalpha << x; }
 void __print(char x) {
   if (x < 31) cerr << static_cast<int>(x);
   else cerr << x;
 }
-template <typename T>
-void __print(optional<T> x) {
+template <typename T> void __print(optional<T> x) {
   if (x) __print(x.value());
   else __print("NONE");
 }
-template <typename A>
-requires std::ranges::range<A>
+template <typename A> requires std::ranges::range<A>
 void __print(const A &x) {
-    bool first = true;
-    cerr << '{';
-    for (const auto &i : x) {
-        cerr << (first ? "" : ","), __print(i);
-        first = false;
-    }
-    cerr << '}';
+  bool first = true;
+  cerr << '{';
+  for (const auto &i : x) {
+    cerr << (first ? "" : ","), __print(i);
+    first = false;
+  }
+  cerr << '}';
 }
-template <typename A, typename B>
-void __print(const pair<A, B> &p) {
-    cerr << '(';
-    __print(p.first);
-    cerr << ',';
-    __print(p.second);
-    cerr << ')';
+template <typename A, typename B> void __print(const pair<A, B> &p) {
+  cerr << '('; __print(p.first);
+  cerr << ','; __print(p.second);
+  cerr << ')';
 }
-template <typename... A>
-void __print(const tuple<A...> &t) {
-    bool first = true;
-    cerr << '(';
-    apply([&first](const auto &...args) { ((cerr << (first ? "" : ","), __print(args), first = false), ...); }, t);
-    cerr << ')';
+template <typename... A> void __print(const tuple<A...> &t) {
+  bool first = true;
+  cerr << '(';
+  apply([&first](const auto &...args) {((cerr << (first ? "" : ","), __print(args), first = false), ...); }, t);
+  cerr << ')';
 }
-template <typename T>
-void __print(stack<T> s) {
-    vector<T> debugVector;
-    while (!s.empty()) {
-        T t = s.top();
-        debugVector.push_back(t);
-        s.pop();
-    }
-    reverse(debugVector.begin(), debugVector.end());
-    __print(debugVector);
+template <typename T> void __print(stack<T> s) {
+  vector<T> debugVector;
+  while (!s.empty()) {
+    T t = s.top();
+    debugVector.push_back(t);
+    s.pop();
+  }
+  reverse(debugVector.begin(), debugVector.end());
+  __print(debugVector);
 }
-template <typename T>
-void __print(queue<T> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.front();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
+template <typename T> void __print(queue<T> q) {
+  vector<T> debugVector;
+  while (!q.empty()) {
+    T t = q.front();
+    debugVector.push_back(t);
+    q.pop();
+  }
+  __print(debugVector);
 }
-template <typename T, typename... U>
-void __print(priority_queue<T, U...> q) {
-    vector<T> debugVector;
-    while (!q.empty()) {
-        T t = q.top();
-        debugVector.push_back(t);
-        q.pop();
-    }
-    __print(debugVector);
+template <typename T, typename... U> void __print(priority_queue<T, U...> q) {
+  vector<T> debugVector;
+  while (!q.empty()) {
+    T t = q.top();
+    debugVector.push_back(t);
+    q.pop();
+  }
+  __print(debugVector);
 }
 void _print() { cerr << "]\n"; }
 template <typename Head, typename... Tail>
 void _print(const Head &H, const Tail &...T) {
-    __print(H);
-    if (sizeof...(T))
-        cerr << ", ";
-    _print(T...);
+  __print(H);
+  if (sizeof...(T)) cerr << ", ";
+  _print(T...);
 }
 
 #define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
