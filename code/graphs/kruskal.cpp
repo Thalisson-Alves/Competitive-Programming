d39af979b35c3bd4b070a0c8db73b7a8f0af3919
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-25 20:26:17 -0300

    update ufds method

--- code/graphs/kruskal.cpp
+++ code/graphs/kruskal.cpp
@@ -1,11 +1,9 @@
 vector<tuple<ll, int, int>> kruskal(int n, vector<tuple<ll, int, int>> &edges) {
   UFDS ufds(n);
   vector<tuple<ll, int, int>> ans;
-
   sort(edges.begin(), edges.end());
-  for (auto [a, b, c] : edges) if (ufds.union_set(b, c)) {
+  for (auto [a, b, c] : edges) if (ufds.join(b, c)) {
     ans.emplace_back(a, b, c);
   }
-
   return ans;
 }
