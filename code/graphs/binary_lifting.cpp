2f48f975bbf2cd8dee81ecbbf00c829d846b4b01
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-25 20:25:48 -0300

    add lca query

--- code/graphs/binary_lifting.cpp
+++ code/graphs/binary_lifting.cpp
@@ -1,46 +1,48 @@
-struct BinaryLifting {
-  vector<int> far, depth, parent;
-  BinaryLifting(const vector<vector<int>> &g, int root=0) : far(size(g), -1), depth(size(g)), parent(size(g), -1) {
+template <typename T, typename Op = T(*)(T,T)> struct BinaryLifting {
+  const int lg;
+  vector<vector<int>> up;
+  vector<vector<T>> acc;
+  vector<int> depth;
+  const Op op;
+  const T e;
+  BinaryLifting(const vector<vector<pair<int, T>>> &g, int root=0, Op f = Op(), T id = T()) : lg(32-__builtin_clz((int)size(g))), up(size(g), vector<int>(lg)), acc(size(up), vector<T>(size(up[0]), id)), depth(size(up)), op(f), e(id) {
     vector<int> q{root};
     q.reserve(size(g));
     for (int i = 0; i < (int)q.size(); i++) {
       auto u = q[i];
-      for (auto v : g[u]) if (v != parent[u]) {
-        parent[v] = u;
-        depth[v] = depth[u] + 1;
-        int p1 = u;
-        int p2 = far[u];
-        if (~p2 and ~far[p2] and depth[p1] - depth[p2] == depth[p2] - depth[far[p2]]) far[v] = far[p2];
-        else far[v] = p1;
+      for (auto [v, w] : g[u]) if (v != up[u][0]) {
         q.push_back(v);
+        up[v][0] = u;
+        acc[v][0] = w;
+        depth[v] = depth[u] + 1;
+        for (int j = 1; j < lg; j++) {
+          up[v][j] = up[up[v][j-1]][j-1];
+          acc[v][j] = op(acc[v][j-1], acc[up[v][j-1]][j-1]);
+        }
       }
     }
   }
-  int kth_parent(int u, int k) const {
-    while (~u and k > 0) {
-      if (~far[u] and depth[u] - k < depth[far[u]]) {
-        k -= depth[u] - depth[far[u]];
-        u = far[u];
-      } else {
-        k--;
-        u = parent[u];
+  pair<int, T> kth_parent(int u, int k) const {
+    T res = e;
+    for (int i = lg-1; ~i; --i) {
+      if (k>>i&1) {
+        res = op(res, acc[u][i]);
+        u = up[u][i];
       }
     }
-    return u;
+    return {u, res};
   }
-  int lca(int u, int v) const {
+  pair<int, T> lca(int u, int v) const {
     if (depth[u] < depth[v]) swap(u, v);
-    while (depth[v] < depth[u]) {
-      if (~far[u] and depth[v] <= depth[far[u]]) u = far[u];
-      else u = parent[u];
+    T res;
+    tie(u, res) = kth_parent(u, depth[u]-depth[v]);
+    if (u == v) return {u, res};
+    for (int i = lg-1; ~i; --i) {
+      if (up[u][i] != up[v][i]) {
+        res = op(res, op(acc[u][i], acc[v][i]));
+        u = up[u][i], v = up[v][i];
+      }
     }
-    if (u == v) return u;
-    while (parent[u] != parent[v]) {
-      if (~far[u] and ~far[v] and far[u] != far[v])
-        u = far[u], v = far[v];
-      else
-        u = parent[u], v = parent[v];
-    }
-    return parent[u];
+    return {up[u][0], op(res, op(acc[u][0], acc[v][0]))};
   }
 };
