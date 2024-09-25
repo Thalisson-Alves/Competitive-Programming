8f85d491266d4ed69e63c0bceb53c68e4ed765d2
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-25 20:26:29 -0300

    update sparse table

--- code/graphs/lca_sparse_table.cpp
+++ code/graphs/lca_sparse_table.cpp
@@ -1,8 +1,8 @@
 struct LCA {
   vector<int> height, euler, first;
+  static pair<int,int> min(pair<int,int> a, pair<int,int> b) { return std::min(a, b); }
   SparseTable<pair<int, int>> table;
-
-  LCA(vector<vector<int>> &g, int root = 0) : height(g.size()), euler(g.size() * 2), first(g.size()), table(F(min(a,b)),{INT_MAX,INT_MAX}) {
+  LCA(vector<vector<int>> &g, int root = 0) : height(g.size()), euler(g.size() * 2), first(g.size()), table(min,{INT_MAX,INT_MAX}) {
     vector<bool> visited(g.size());
     dfs(g, root, visited);
     int m = (int)euler.size();
@@ -10,7 +10,6 @@ struct LCA {
     for (int i = 0; i < m; i++) v[i] = {height[euler[i]], euler[i]};
     table.build(v);
   }
-
   void dfs(vector<vector<int>> &adj, int node, vector<bool> &visited, int h = 0) {
     visited[node] = true;
     height[node] = h;
@@ -23,7 +22,6 @@ struct LCA {
       }
     }
   }
-
   int lca(int u, int v) {
     int left = first[u], right = first[v];
     if (left > right) swap(left, right);
