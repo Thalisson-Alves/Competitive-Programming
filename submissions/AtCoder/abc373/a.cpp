a9d1f194dd510d802e9f7b8544c7d925f588293e
Thalisson-Alves <thalisson2030@gmail.com>
2024-09-29 00:50:16 -0300

    adhoc

---
+++ submissions/AtCoder/abc373/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+
+void solve() {
+  int res = 0;
+  for (int i = 1; i <= 12; i++) {
+    string s;
+    cin >> s;
+    res += (int)s.size() == i;
+  }
+  cout << res << '\n';
+}
+
+int32_t main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int t = 1;
+  // cin >> t; cin.ignore();
+  for (auto i = 1; i <= t; i++) {
+    solve();
+  }
+}
