be3f80a9c21ec39a9076831fe196eaf9e1a6d27e
Thalisson-Alves <thalisson2030@gmail.com>
2024-10-02 21:28:55 -0300

    update kmp

--- code/strings/kmp.cpp
+++ code/strings/kmp.cpp
@@ -1,28 +1,18 @@
-vector<int> kmp(const string &p) {
-  int m = (int)p.size(), t = -1;
-  vector<int> bs(m + 1, -1);
-
-  for (int i = 1; i <= m; i++) {
-    for (; t > -1 and p[t] != p[i - 1]; t = bs[t]);
-    ++t;
-    bs[i] = (i == m or p[t] != p[i]) ? t : bs[t];
+vector<int> kmp(const string &s) {
+  vector<int> pi(size(s));
+  for (int i = 1; i < (int)size(s); i++) {
+    int j = pi[i-1];
+    for (; j and s[i]!=s[j]; j=pi[j-1]);
+    pi[i] = j+(s[i]==s[j]);
   }
-
-  return bs;
+  return pi;
 }
-
 int count_occurences(const string &s, const string &p) {
-  int n = (int)s.size(), m = (int)p.size(), i = 0, j = 0, occ = 0;
-  auto bs = kmp(p);
-
-  while (i <= n - m) {
-    for (; j < m and p[j] == s[i + j]; ++j);
-    if (j == m) ++occ;
-
-    int shift = j - bs[j];
-    i += shift;
-    j = max(0, j - shift);
+  int m = (int)p.size(), occ = 0;
+  auto pi = kmp(p);
+  for (int i = 0, j = 0; i < (int)size(s); i++) {
+    for (; j and (j==m or s[i]!=p[j]); j=pi[j-1]);
+    if (s[i] == p[j]) occ += ++j == m;
   }
-
   return occ;
 }
