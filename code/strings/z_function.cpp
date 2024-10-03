284817c1bba81d9bf9995b30295899bc28340f85
Thalisson-Alves <thalisson2030@gmail.com>
2024-10-02 21:29:01 -0300

    refactor

--- code/strings/z_function.cpp
+++ code/strings/z_function.cpp
@@ -2,16 +2,9 @@ template <typename T> vector<int> z_function(const T &s) {
   vector<int> z(s.size());
   int l = 0, r = 0;
   for(int i = 1; i < (int)s.size(); i++) {
-    if(i < r) {
-      z[i] = min(r - i, z[i - l]);
-    }
-    while(i + z[i] < (int)s.size() && s[z[i]] == s[i + z[i]]) {
-      z[i]++;
-    }
-    if(i + z[i] > r) {
-      l = i;
-      r = i + z[i];
-    }
+    if (i < r) z[i] = min(r - i, z[i - l]);
+    for (; i + z[i] < (int)s.size() && s[z[i]] == s[i + z[i]]; z[i]++);
+    if (i + z[i] > r) l = i, r = i + z[i];
   }
   return z;
 }
