430ab4b3e66c57e4b0648470a94f5b92d2573b7b
Thalisson-Alves <thalisson2030@gmail.com>
2024-10-12 13:51:42 -0300

    refactor

--- code/graphs/find_bridges.cpp
+++ code/graphs/find_bridges.cpp
@@ -1,26 +1,20 @@
-template <typename T> void find_bridges(Undigraph<T> &g, auto &&f) {
+template <typename Undigraph> void find_bridges(Undigraph &g, auto &&f) {
   static_assert(std::is_invocable_r_v<void, decltype(f), int, int>);
-
   vector<int> tin(g.size()), tout(g.size()), low(g.size());
   int timer = 1;
-
   auto dfs = [&](auto &&self, int u, int p) -> void {
     tin[u] = low[u] = timer++;
-
-    for (auto [v, _] : g[u]) if (v != p) {
+    for (auto v : g[u]) if (v != p) {
       if (tin[v]) {
         low[u] = min(low[u], tin[v]);
       } else {
         self(self, v, u);
         low[u] = min(low[u], low[v]);
-        if (low[v] > tin[u]) {
-          f(u, v);
-        }
+        if (low[v] > tin[u]) f(u, v);
       }
     }
   };
-
-  for (int i = 0; i < g.size(); i++) {
+  for (int i = 0; i < (int)g.size(); i++) {
     if (!tin[i])
       dfs(dfs, i, -1);
   }
