9aaba77398279d404050d102ebc737d2e6b56feb
Thalisson-Alves <thalisson2030@gmail.com>
2024-10-16 18:27:01 -0300

    use typename T instead of ll

--- code/math/divisors.cpp
+++ code/math/divisors.cpp
@@ -1,6 +1,6 @@
-vector<ll> divisors(ll n) {
-  vector<ll> d;
-  for (ll i = 1; i*i <= n; i++) if (n%i == 0) {
+template <typename T> vector<T> divisors(T n) {
+  vector<T> d;
+  for (T i = 1; (ll)i*i <= n; i++) if (n%i == 0) {
     d.push_back(i);
     if (n/i != i) d.push_back(n/i);
   }
