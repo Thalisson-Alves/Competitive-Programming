c782c3ac6860627100e0d7da4d2e4bbef1c6004b
Thalisson-Alves <thalisson2030@gmail.com>
2024-11-05 20:42:47 -0300

    rename ufds to dsu

--- code/data_structures/dsu.cpp
+++ code/data_structures/dsu.cpp
@@ -1,10 +1,8 @@
-struct UFDS {
+struct DSU {
   vector<int> ps, sz;
-  int components;
-  UFDS(int n = 0) : ps(n), sz(n, 1), components(n) {
-    iota(ps.begin(), ps.end(), 0);
-  }
-  int find(int x) { return (x == ps[x] ? x : (ps[x] = find(ps[x]))); }
+  int comp;
+  DSU(int n = 0) : ps(n, -1), sz(n, 1), comp(n) {}
+  int find(int x) { return (~ps[x] ? (ps[x] = find(ps[x])) : x); }
   bool same(int x, int y) { return find(x) == find(y); }
   bool join(int x, int y) {
     x = find(x);
@@ -13,14 +11,14 @@ struct UFDS {
     if (sz[x] < sz[y]) swap(x, y);
     ps[y] = x;
     sz[x] += sz[y];
-    --components;
+    --comp;
     return true;
   }
   int add() {
     int id = (int)ps.size();
     ps.push_back(id);
     sz.push_back(1);
-    ++components;
+    ++comp;
     return id;
   };
 };
