2088902f7511999811c881c8998cf414137dd175
Thalisson-Alves <thalisson2030@gmail.com>
2024-11-16 15:03:22 -0300

    add ordered_map

--- code/data_structures/ordered_set.cpp
+++ code/data_structures/ordered_set.cpp
@@ -1,6 +1,10 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
-template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename K, typename V, typename Comp = less<K>>
+using ordered_map = tree<K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename K, typename Comp = less<K>>
+using ordered_set = ordered_map<K, null_type, Comp>;
+// Supports
+// - find_by_order(idx): returns an iterator to the idx-th element (0-indexed)
+// - order_of_key(key): returns the index of the key in the set
