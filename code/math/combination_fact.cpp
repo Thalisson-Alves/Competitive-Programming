eb4d49fe251c10c8792e65fb03c0fd06261e3cd1
Thalisson-Alves <thalisson2030@gmail.com>
2024-11-22 21:08:24 -0300

    add factorial inverse

--- code/math/combination_fact.cpp
+++ code/math/combination_fact.cpp
@@ -1,13 +1,21 @@
 template <typename T=mint> T factorial(int n) {
-  static vector<T> fact{1};
+  static vector<T> fact{1, 1};
   while ((int)size(fact) <= n) fact.push_back(fact.back() * size(fact));
   return fact[n];
 }
+template <typename T=mint> T factorial_inv(int n) {
+  static vector<T> inv{1, 1}, finv{1, 1};
+  while ((int)size(inv) <= n) {
+    inv.push_back(T::mod - T::mod / size(inv) * inv[T::mod % size(inv)].value() % T::mod);
+    finv.push_back(finv.back() * inv.back());
+  }
+  return finv[n];
+}
 template <typename T=mint> T comb(int n, int k) {
   if (n < k) return 0;
-  return factorial<T>(n) / (factorial<T>(n-k) * factorial<T>(k));
+  return factorial<T>(n) * factorial_inv<T>(n-k) * factorial_inv<T>(k);
 }
 // same as comb(2*n, n) / (n + 1)
 template <typename T=mint> T catalan(int n) {
-  return factorial<T>(2*n) / (factorial<T>(n) * factorial<T>(n + 1));
+  return factorial<T>(2*n) * factorial_inv<T>(n) * factorial_inv<T>(n + 1);
 }
